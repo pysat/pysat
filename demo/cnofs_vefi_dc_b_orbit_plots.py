18ce7514ebeb02f1637cc28a9bb31fbb71e93b1e
rstone <rstoneba@utdallas.edu>
2015-04-14 17:49:40 -0500

    Improved documentation within file

--- demo/cnofs_vefi_dc_b_orbit_plots.py
+++ demo/cnofs_vefi_dc_b_orbit_plots.py
@@ -18,12 +18,17 @@ vefi = pysat.Instrument(name='cnofs_vefi', tag='dc_b', clean_level=None,
 
 # set limits on dates analysis will cover, inclusive
 # leave bounds unassigned to cover the whole dataset (comment out lines below)
-start = pds.datetime(2008,5,9)
-stop = pds.datetime(2014, 9, 30)
-vefi.bounds = (start,stop)
+#start = pds.datetime(2010,5,9)
+#stop = pds.datetime(2010,5,12)
+#vefi.bounds = (start,stop)
+
+# if there is no vefi dc magnetometer data on your system, then run command below
+# where start and stop are pandas datetimes (from above)
+# pysat will automatically register the addition of this data at the end of download
+#vefi.download(start, stop)
 
 for orbit_count, vefi in enumerate(vefi.orbits):
-    # for each loop pysat puts a copy of a particular orbit's data into vefi.data
+    # for each loop pysat puts a copy of the next available orbit into vefi.data
     # changing .data at this level does not alter other orbits
     # reloading the same orbit will erase any changes made
     
@@ -66,6 +71,7 @@ for orbit_count, vefi in enumerate(vefi.orbits):
     ax[6].set_xlabel(vefi.meta['longitude'].long_name)
     ax[6].set_xticks([0,60,120,180,240,300,360])
     ax[6].set_xlim((0,360))   
+    
     f.tight_layout()
     plt.savefig(os.path.join(results_dir,'orbit_%05i.png' % orbit_count ) )
     plt.close()
