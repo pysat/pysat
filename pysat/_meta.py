4eb734dca862dede6e3f2335e3f51ef765dffff8
rstone <rstoneba@utdallas.edu>
2015-04-25 17:39:24 -0500

    Improved docstring and supports partial metadata assignment

--- pysat/_meta.py
+++ pysat/_meta.py
@@ -5,16 +5,18 @@ from pysat import DataFrame, Series
 
 class Meta(object):
     """
-    Stores metadata for an associated pysat Satellite instance. 
-    Similar to CF-1.6 data standard info.
+    Stores metadata for associated pysat Instrument instance, similar to CF-1.6 data standard.
     
-    inputs:
-        metadata: pandas DataFrame indexed by variable name that contains
-         atleast the standard_name (name), units, and long_name for the data 
-         stored in the associated pysat Satellite object.
+    Parameters
+    ----------
+    metadata : pandas.DataFrame 
+        DataFrame should be indexed by variable name that contains at minimum the 
+        standard_name (name), units, and long_name for the data stored in the associated 
+        pysat Instrument object.
+        
     """
     def __init__(self, metadata=None):
-        self.replace(metadata=metadata)    
+        self.replace(metadata=metadata)  
 
     def __eq__(self, other):
         if type(other) is type(self):
@@ -30,9 +32,17 @@ class Meta(object):
         """add metadata information"""
         if isinstance(value,dict):
             if 'units' not in value.keys():
-                raise ValueError('Must provide "units" in input dictionary.')
+                if name not in self.data.index:
+                    value['units'] = ''
+                else:
+                    value['units'] = self.data.ix[name,'units']
+            #    raise ValueError('Must provide "units" in input dictionary.')
             if 'long_name' not in value.keys():
-                value['long_name'] = name  
+                if name not in self.data.index:
+                    # only set default if name not already in meta
+                    value['long_name'] = name  
+                else:
+                    value['long_name'] = self.data.ix[name, 'long_name']
                 
             if hasattr(value[value.keys()[0]], '__iter__' ):
                 # an iterable of things         
@@ -58,12 +68,14 @@ class Meta(object):
     #    self.data = self._orig_data
         
     def replace(self, metadata=None):
-        """Replace metadata and saved metadata with input data.
+        """Replace stored metadata with input data.
         
-        inputs:
-            metadata: pandas DataFrame indexed by variable name that contains
-            standard_name(name), units, and long_name for the data stored in 
-            the associated pysat Satellite object.
+        Parameters
+        ----------
+        metadata : pandas.DataFrame 
+            DataFrame should be indexed by variable name that contains at minimum the 
+            standard_name (name), units, and long_name for the data stored in the associated 
+            pysat Instrument object.
         """
         if metadata is not None:
             if isinstance(metadata, DataFrame):
@@ -79,7 +91,21 @@ class Meta(object):
     @classmethod
     def from_csv(cls, name=None, col_names=None, sep=None, **kwargs):
         """
-        Create satellite metadata object from csv
+        Create satellite metadata object from csv.
+        
+        Parameters
+        ----------
+        name : string
+            absolute filename for csv file or name of instrument if csv information
+            stored in pandas instrument location
+        col_names : list-like collection of strings
+            column names in csv and resultant meta object
+        sep : string
+            column seperator for supplied csv filename
+
+        Note
+        ----
+        column names must include at least ['name', 'long_name', 'units'].
         """
         import pysat
         req_names = ['name','long_name','units']
@@ -95,8 +121,7 @@ class Meta(object):
             raise ValueError('Must supply an instrument name or file path.')
         elif not isinstance(name, str):
             raise ValueError('keyword name must be related to a string')               
-        else:
-            if not os.path.isfile(name):
+        elif not os.path.isfile(name):
                 # Not a real file, assume input is a pysat instrument name
                 # and look in the standard pysat location.
                 test =  os.path.join(pysat.__path__,'instruments',name+'_meta.txt')
@@ -112,20 +137,21 @@ class Meta(object):
                     else:
                         #success
                         name = test
-                                     
-        mdata = pds.read_csv(name, names=col_names, sep=sep, **kwargs) 
+        else:                             
+            mdata = pds.read_csv(name, names=col_names, sep=sep, **kwargs) 
         if not mdata.empty:
-            #make sure the data name is the index
+            # make sure the data name is the index
             mdata.index = mdata['name']
             return cls(metadata=mdata)
         else:
             raise ValueError('Unable to retrieve information from ' + name)
         
-    
+    @classmethod
     def from_nc():
-        """load metadata from netCDF"""
+        """not implemented yet, load metadata from netCDF"""
         pass
-    
+        
+    @classmethod
     def from_dict():
-        """load metadata from dict of items/list types"""
+        """not implemented yet, load metadata from dict of items/list types"""
         pass
