dbe08f5618549939bb54069bd4eb775c6ceeba92
rstone <rstoneba@utdallas.edu>
2015-04-27 10:22:30 -0500

    updated to work for author

--- docs/conf.py
+++ docs/conf.py
@@ -19,9 +19,8 @@ import shlex
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../pysat'))
-
-#import pysat
+sys.path.insert(0, os.path.abspath('../'))
+import pysat
 # supported instruments aren't automatically import by pysat
 # so, get a list of .py files and import them here
 #import os.path, pkgutil
