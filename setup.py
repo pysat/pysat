00ba1c54dc148b1b1200334b43b65037e087cf90
rstone <rstoneba@utdallas.edu>
2015-04-29 00:10:50 -0500

    Corrected python support (2.7); incremented version to reflect change in setup.py; will push to pypi

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2.0',
+    version='0.2.1',
 
     description='Supports science data analysis across measurement platforms',    
     #long_description=(''),
@@ -54,8 +54,8 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
+        #'Programming Language :: Python :: 2',
+        #'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         #'Programming Language :: Python :: 3',
         #'Programming Language :: Python :: 3.2',
