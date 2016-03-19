a27b57d1ef68e46924976a17497110bdfc5de130
rstone <rstoneba@utdallas.edu>
2016-03-19 13:21:44 -0500

    Added longer description.

--- setup.py
+++ setup.py
@@ -16,16 +16,19 @@ here = path.abspath(path.dirname(__file__))
 #with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
 #    long_description = f.read()
 
+with open(path.join(here,'description.txt')) as f:
+    long_description = f.read()
+
 setup(
     name='pysat',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.4.1',
+    version='0.4.2.dev',
 
     description='Supports science data analysis across measurement platforms',    
-    #long_description=(''),
+    long_description=long_description,
     # The project's main homepage.
     url='http://github.com/rstoneback/pysat',
 
@@ -42,7 +45,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: Science/Research',
@@ -60,7 +63,7 @@ setup(
         #'Programming Language :: Python :: 3',
         #'Programming Language :: Python :: 3.2',
         #'Programming Language :: Python :: 3.3',
-        #'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.4',
     ],
 
     # What does your project relate to?
