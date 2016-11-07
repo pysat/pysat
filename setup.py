4e8152c0f4cda26d1a3d9be70f8356d172d6c38c
rstone <rstoneba@utdallas.edu>
2016-11-07 11:36:45 -0600

    Updated version number

--- setup.py
+++ setup.py
@@ -5,17 +5,12 @@ https://github.com/pypa/sampleproject
 """
 
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages
+from setuptools import setup
 # To use a consistent encoding
 from codecs import open
 from os import path
 
 here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the relevant file
-#with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
-#    long_description = f.read()
-
 with open(path.join(here,'description.txt'), encoding='utf-8') as f:
     long_description = f.read()
 
@@ -25,7 +20,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.4.3',
+    version='0.4.4.dev0',
     
     description='Supports science data analysis across measurement platforms',    
     long_description=long_description,
@@ -71,45 +66,11 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=['pysat','pysat.instruments', 'pysat.ssnl', 'pysat.tests'],
+    packages=['pysat','pysat.instruments', 'pysat.ssnl'],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires = ['pandas'] #, 'pysftp'],
-    #install_requires=['pandas'], #,'git+git://github.com/rstoneback/pandas/tarball/master#egg=rstoneback-pandas-0.16.0-54-ga32da86'],
-    #dependency_links = ['https://github.com/rstoneback/pandas/tarball/master#egg=rstoneback-pandas-0.16.0-54-ga32da86'],
-    #dependency_links = ['https://github.com/rstoneback/pandas.git'],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    #extras_require={
-    #    'dev': ['check-manifest'],
-    #    'test': ['coverage'],
-    #},
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    #package_data={
-    #    'pysat.instruments': ['cnofs_ivm_meta.txt'],
-    #},
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    #entry_points={
-    #    'console_scripts': [
-    #        'sample=sample:main',
-    #    ],
-    #},
+    install_requires = ['pandas'] #'matplotlib'
 )
