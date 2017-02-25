811fd224842930b22e0c5ba01242ed5b0a6a1ed0
Russell Stoneback <rstoneba@utdallas.edu>
2017-02-24 19:55:38 -0600

    Updated version number

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.4.4',
+    version='0.4.5.dev0',
     
     description='Supports science data analysis across measurement platforms',    
     long_description=long_description,
