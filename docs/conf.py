caedd1692b586f918b13f87c59515ae3c49d7b73
Russell Stoneback <rstoneba@utdallas.edu>
2017-04-03 03:19:34 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ import shlex
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
-sys.path.insert(0,os.path.abspath('.'))
+sys.path.insert(0,os.path.abspath('..'))
 #print sys.path
 import pysat
 #print pysat.__path__
