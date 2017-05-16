03487bc32cccf8d5c91e5d522c15ab53a6d59b08
rstoneback <rstoneba@utdallas.edu>
2017-05-15 22:57:17 -0500

    Updated to prevent creation of directories in cwd when testing.

--- pysat/tests/test_utils.py
+++ pysat/tests/test_utils.py
@@ -8,6 +8,7 @@ import nose.tools
 import pysat.instruments.pysat_testing
 import numpy as np
 import os
+import tempfile
 
 import sys
 if sys.version_info[0] >= 3:
@@ -32,13 +33,8 @@ def prep_dir(inst=None):
         #print ('Made Directory')
     except OSError:
         pass
-    #dir = os.path.join(pysat.data_dir, inst.platform)
-    #if not os.path.isdir(dir):
-    #    os.mkdir(dir)
-    #dir = os.path.join(pysat.data_dir, inst.platform, inst.name)
-    #if not os.path.isdir(dir):
-    #    os.mkdir(dir)
 
+def remove_files(inst):
     # remove any files
     dir = inst.files.data_path
     for the_file in os.listdir(dir):
@@ -82,11 +78,25 @@ def prep_dir(inst=None):
 class TestBasics:
     def setup(self):
         """Runs before every method to create a clean testing setup."""
-        self.testInst = pysat.Instrument(inst_module=pysat.instruments.pysat_testing, clean_level='clean')
+        # store current pysat directory
+        self.data_path = pysat.data_dir
+        
+        # create temporary directory  
+        dir_name = tempfile.gettempdir()
+        pysat.utils.set_data_dir(dir_name, store=False)
+
+        self.testInst = pysat.Instrument(inst_module=pysat.instruments.pysat_testing, 
+                                        clean_level='clean')
+        # create testing directory
+        prep_dir(self.testInst)
 
     def teardown(self):
         """Runs after every method to clean up previous testing."""
-        prep_dir()
+        remove_files(self.testInst)
+        try:
+            pysat.utils.set_data_dir(self.data_path, store=False)
+        except:
+            pass
         del self.testInst
     
     def test_basic_writing_and_reading_netcdf4_default_format(self):
@@ -155,7 +165,7 @@ class TestBasics:
 
 
     def test_set_data_dir(self):
-        saved_dir = pysat.data_dir
+        saved_dir = self.data_path
         # update data_dir
         pysat.utils.set_data_dir('.')
         check1 = (pysat.data_dir == '.')
@@ -178,16 +188,17 @@ class TestBasics:
         else:
             re_load = reload
 
-        saved_dir = pysat.data_dir
+        saved_dir = self.data_path #pysat.data_dir
         # update data_dir
         pysat.utils.set_data_dir('.', store=False)
         check1 = (pysat.data_dir == '.')
         pysat._files = re_load(pysat._files)
         pysat._instrument = re_load(pysat._instrument)
+        re_load(pysat)
 
         check2 = (pysat.data_dir == saved_dir)
         if saved_dir is not '':
-            pysat.utils.set_data_dir(saved_dir)
+            pysat.utils.set_data_dir(saved_dir, store=False)
             check3 = (pysat.data_dir == saved_dir)
         else:
             check3 = True
