1f8a64b2c6cb313eb643349a1f61b2adf29b58a2
Russell Stoneback <rstoneba@utdallas.edu>
2017-05-17 02:09:11 -0500

    Update __init__.py

--- pysat/instruments/__init__.py
+++ pysat/instruments/__init__.py
@@ -3,5 +3,5 @@
 
 
 __all__ = ['cosmic2013_gps', 'cnofs_vefi', 'cnofs_ivm', 'cnofs_plp', 'cosmic_gps', 
-           'superdarn_grdex', 'omni_hro', 'rocsat1_ivm', 'champ_star', 'sw_dst', 'sw_kp']
+           'superdarn_grdex', 'omni_hro', 'rocsat1_ivm', 'champ_star', 'sw_dst', 'sw_kp', 'timed_see']
 from . import *
