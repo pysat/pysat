c96c8e839deff2127379d493e41ff52392fc177a
Carey Spence <jcspence@users.noreply.github.com>
2018-02-23 14:13:20 -0600

    Created Constellation class and stub constructor.

---
+++ pysat/_constellation.py
@@ -0,0 +1,18 @@
+
+class Instrument(object):
+    """Manage and analyze data from multiple pysat Instruments.
+
+    FIXME document this.
+    """
+    def __init__(self, instruments = None, name = None):
+        if instruments and name:
+            raise ValueError('When creating a constellation, please specify a '
+                             'list of instruments or a name, not both.')
+        elif instruments and not hasattr(instruments, '__getitem__'):
+            raise ValueError('Constellation: Instruments must be list-like.')
+        elif not (name or instruments):
+            raise ValueError('Constellation: Cannot create empty constellation.')
+
+        # FIXME
+        pass
+    
