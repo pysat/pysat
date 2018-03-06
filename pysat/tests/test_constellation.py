28efcf222b28cd7d4a5b0443d92c94b2c347237e
Carey Spence <jcspence@users.noreply.github.com>
2018-03-05 20:13:20 -0600

    style improvements

--- pysat/tests/test_constellation.py
+++ pysat/tests/test_constellation.py
@@ -1,48 +1,67 @@
+from nose.tools import raises
 import pysat
-from nose.tools import assert_raises, raises
 
 # TODO
 
+
 class TestConstellation:
+    """Test the Constellation class."""
     def setup(self):
-        self.instruments = [ pysat.Instrument('pysat','testing', clean_level='clean') 
-                             for i in range(2) ] 
+        """Create instruments and a constellation for each test."""
+        self.instruments = [pysat.Instrument('pysat', 'testing',
+                                             clean_level='clean')
+                            for i in range(2)]
         self.const = pysat.Constellation(self.instruments)
 
     def teardown(self):
+        """Clean up after each test."""
         del self.const
 
     def test_construct_by_list(self):
-        pass
+        """Construct a Constellation with a list."""
+        const = pysat.Constellation(self.instruments)
+        assert len(const.instruments) == 2
 
     def test_construct_by_name(self):
-        self.const = pysat.Constellation(name='testing')
-        assert len(self.const.instruments) == 5
+        """Construct a Constellation by name.
+
+        Should access a predefined Constellation."""
+        const = pysat.Constellation(name='testing')
+        assert len(const.instruments) == 5
 
     @raises(ValueError)
     def test_construct_both(self):
+        """Attempt to construct a Constellation by name and list.
+        Raises an error."""
         pysat.Constellation(
-            instruments = self.instruments,
-            name = 'testing');
+            instruments=self.instruments,
+            name='testing')
 
     @raises(ValueError)
     def test_construct_bad_instruments(self):
-        pysat.Constellation(instruments = 42)
+        """Attempt to construct a Constellation with
+        a bad instrument 'list.'"""
+        pysat.Constellation(instruments=42)
 
     @raises(ValueError)
     def test_construct_null(self):
+        """Attempt to construct a Constellation with
+        no arguments."""
         pysat.Constellation()
 
     def test_getitem(self):
+        """Test Constellation:__getitem__."""
         assert self.const[0] == self.instruments[0]
         assert self.const[1] == self.instruments[1]
         assert self.const[:] == self.instruments[:]
 
     def test_str(self):
+        """Test Constellation:__str__."""
         # FIXME define that string.
         assert str(self.const) == "stringgoeshere"
 
     def test_repr(self):
+        """Test Constellation:__repr__."""
         # FIXME
         print(repr(self.const))
         assert repr(self.const) == "Constellation([..."
