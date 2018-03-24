7b09a4d40f9d73a8eef770dca4770ea2c91f3b39
rstoneback <rstoneba@utdallas.edu>
2018-03-23 19:01:40 -0500

    Initial upload for ICON EUV support

--- pysat/instruments/__init__.py
+++ pysat/instruments/__init__.py
@@ -3,7 +3,7 @@
 
 
 __all__ = ['champ_star', 'cnofs_ivm', 'cnofs_plp', 'cnofs_vefi', 'cosmic2013_gps', 'cosmic_gps',
-           'icon_ivm', 'iss_fpmu',
+           'icon_ivm', 'icon_euv', 'iss_fpmu',
            'omni_hro', 'rocsat1_ivm', 'superdarn_grdex', 'sw_dst', 'sw_kp', 'timed_see',
            ]
 from . import *
