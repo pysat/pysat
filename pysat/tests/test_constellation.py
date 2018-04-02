032a2e4c72b893bbdcfa6eddb47871a7a4cefea5
Carey Spence <jcspence@users.noreply.github.com>
2018-04-02 16:17:08 -0500

    pysat/tests/test_constellation.py

--- pysat/tests/test_constellation.py
+++ pysat/tests/test_constellation.py
@@ -71,10 +71,11 @@ class TestConstellation:
 class TestAdditionIdenticalInstruments:
     def setup(self):
         insts = []
-        for i in range(3):
+        for _ in range(3):
             insts.append(pysat.Instrument('pysat', 'testing', clean_level='clean'))
         self.const1 = pysat.Constellation(insts)
-        self.const2 = pysat.Constellation([pysat.Instrument('pysat', 'testing', clean_level='clean')])
+        self.const2 = pysat.Constellation(
+                        [pysat.Instrument('pysat', 'testing', clean_level='clean')])
 
     def teardown(self):
         del self.const1
@@ -82,10 +83,10 @@ class TestAdditionIdenticalInstruments:
 
     def test_addition_identical(self):
         for inst in self.const1:
-            inst.bounds = (pysat.datetime(2008,1,1), pysat.datetime(2008,2,1))
+            inst.bounds = (pysat.datetime(2008, 1, 1), pysat.datetime(2008, 2, 1))
         for inst in self.const2:
-            inst.bounds = (pysat.datetime(2008,1,1), pysat.datetime(2008,2,1))
-        
+            inst.bounds = (pysat.datetime(2008, 1, 1), pysat.datetime(2008, 2, 1))
+
         bounds1 = [0,360]
         label1 = 'longitude'
         bounds2 = [-90,90]
@@ -104,7 +105,7 @@ class TestAdditionIdenticalInstruments:
 class TestAdditionOppositeInstruments:
     def setup(self):
         """
-        The data in testadd1['dummy1'] is just ascending integers 0 to the 
+        The data in testadd1['dummy1'] is just ascending integers 0 to the
         length of the other data, testadd2 has the same data but negative.
         The addition of these two signals should be zero everywhere.
         """
@@ -137,7 +138,7 @@ class TestAdditionSimilarInstruments:
     def setup(self):
         """
         All the data in dummy1 of testadd3 is the data in testadd1 + 10
-        So the addition of testadd1 and testadd3 should be no more than 10 off from 
+        So the addition of testadd1 and testadd3 should be no more than 10 off from
         the addition of just testadd1
         TODO: actually check the math on this
         """
@@ -160,9 +161,9 @@ class TestAdditionSimilarInstruments:
         bins3 = [0,24,24]
         label3 = 'mlt'
         data_label = 'dummy1'
-        results = self.testC.add(bounds1, label1, bounds2, label2, bins3, label3, 
+        results = self.testC.add(bounds1, label1, bounds2, label2, bins3, label3,
                 data_label)
-        refresults = self.refC.add(bounds1, label1, bounds2, label2, bins3, label3, 
+        refresults = self.refC.add(bounds1, label1, bounds2, label2, bins3, label3,
                 data_label)
         med = results['dummy1']['median']
         refmed = refresults['dummy1']['median']
@@ -174,7 +175,7 @@ class TestAdditionSimilarInstruments:
 class TestAdditionSingleInstrument:
     def setup(self):
         """
-        The constellation consists of a single instrument, so performing 
+        The constellation consists of a single instrument, so performing
         addition on it should just return the instrument's data within
         the bounds
         """
