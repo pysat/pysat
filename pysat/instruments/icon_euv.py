adf08847637a61d3af570ebca1a405b5df9573b0
rstoneback <rstoneba@utdallas.edu>
2018-04-27 07:38:20 -0500

    Added labels for ICON EUV metadata standards.

--- pysat/instruments/icon_euv.py
+++ pysat/instruments/icon_euv.py
@@ -64,7 +64,13 @@ def load(fnames, tag=None, sat_id=None):
 
     """
     
-    return pysat.utils.load_netcdf4(fnames, epoch_name='Epoch')
+    return pysat.utils.load_netcdf4(fnames, epoch_name='Epoch', 
+                                    units_label='Units', name_label='Long_Name', 
+                                    notes_label='Var_Notes', desc_label='CatDesc',
+                                    plot_label='FieldNam', axis_label='axis', 
+                                    scale_label='scale',
+                                    min_label='ValidMin', max_label='ValidMax',
+                                    fill_label='FillVal')
 
 
 def list_files(tag=None, sat_id=None, data_path=None, format_str=None):
