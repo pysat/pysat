cbcb7176267cbc1b9c75575eb13818c04dc5c6ef
rstoneback <rstoneba@utdallas.edu>
2018-10-09 23:01:06 -0500

    Added F107

--- pysat/instruments/__init__.py
+++ pysat/instruments/__init__.py
@@ -5,7 +5,7 @@
 __all__ = ['champ_star', 'cnofs_ivm', 'cnofs_plp', 'cnofs_vefi',
            'cosmic2013_gps', 'cosmic_gps', 'dmsp_ivm', 'icon_ivm', 'icon_euv',
            'iss_fpmu', 'supermag_magnetometer', 'omni_hro', 'rocsat1_ivm',
-           'superdarn_grdex', 'sw_dst', 'sw_kp', 'timed_see',
+           'superdarn_grdex', 'sw_dst', 'sw_f107', 'sw_kp', 'timed_see',
            'pysat_sgp4', 'sport_ivm', 'demeter_iap']
 
 from . import *
