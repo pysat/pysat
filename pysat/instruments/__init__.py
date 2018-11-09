f0a52b49daad8bbca5471d3f3f89a310c73f0bb8
jklenzing <jklenzing@gmail.com>
2018-11-09 15:30:22 -0500

    initial help docstring for instruments

--- pysat/instruments/__init__.py
+++ pysat/instruments/__init__.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
+"""
+pysat.instruments is a pysat package that provides
+the interface for pysat to download, load, manage,
+modify and analyze science data.  Each instrument
+is contained within a subpackage of this set.
+"""
 
 
 
 __all__ = ['champ_star', 'cnofs_ivm', 'cnofs_plp', 'cnofs_vefi',
-           'cosmic2013_gps', 'cosmic_gps', 'demeter_iap', 
+           'cosmic2013_gps', 'cosmic_gps', 'demeter_iap',
            'dmsp_ivm', 'icon_ivm', 'icon_euv', 'icon_fuv', 'icon_mighti',
            'iss_fpmu',  'omni_hro', 'pysat_sgp4', 'rocsat1_ivm',
-           'sport_ivm', 'superdarn_grdex', 'supermag_magnetometer', 
+           'sport_ivm', 'superdarn_grdex', 'supermag_magnetometer',
            'sw_dst', 'sw_kp', 'timed_see',
            'ucar_tiegcm',]
 
