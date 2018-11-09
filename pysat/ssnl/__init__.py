7febc562021408ec16bbeb3b352626ae77d2ab77
jklenzing <jklenzing@gmail.com>
2018-11-09 15:41:43 -0500

    initial help docstring for ssnl

--- pysat/ssnl/__init__.py
+++ pysat/ssnl/__init__.py
@@ -1,3 +1,18 @@
+"""
+pysat.ssnl is a pysat package that provides
+the interface to perform seasonal analysis on
+data managed by pysat.  These analysis methods
+are independent of instrument type.
+
+Main Features
+-------------
+- Seasonal averaging routine for 1D and 2D data.
+- Occurrence probability routines, daily or by orbit.
+- Scatterplot of data_label(s) as functions of labelx,y
+    over a season.
+
+"""
+
 from . import occur_prob
 from . import avg
 from . import plot
