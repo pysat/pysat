9a7a10103e3ebbdc28fdb2beb443a3db3f020594
jklenzing <jklenzing@gmail.com>
2018-11-09 15:36:34 -0500

    initial help docstring for constellations

--- pysat/constellations/__init__.py
+++ pysat/constellations/__init__.py
@@ -1,1 +1,9 @@
+"""
+pysat.constellations is a pysat package that provides
+the interface for pysat to manage and analyze data
+from multiple pysat Instruments.  Each instrument
+is contained within a subpackage of the pysat.instruments
+package.
+"""
+
 __all__ = ['icon']
