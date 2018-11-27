5b77849512ad78996606430094ec1917a99a9ac7
jklenzing <jklenzing@gmail.com>
2018-11-27 09:19:11 -0500

    Model after examples in _instrument.py

--- test_ix.py
+++ test_ix.py
@@ -7,4 +7,17 @@ date = pysat.datetime(2011,6,29)
 ivm = pysat.Instrument(platform='cnofs', name='ivm',clean_level='none')
 ivm.load(date=date)
 
-print(ivm['Ni',0:10])
+# By name
+print(ivm['Ni'])
+# By position
+print(ivm[0,'Ni'])
+# Slicing by row
+print(ivm[0:10,'Ni'])
+'''
+# By Date
+inst[datetime, 'name']
+# Slicing by date, inclusive
+inst[datetime1:datetime2, 'name']
+# Slicing by name and row/date
+inst[datetime1:datetime1, 'name1':'name2']
+'''
