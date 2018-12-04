5699267f0a55e9fd8230a7b8f5638e18ed93ca12
rstoneback <rstoneba@utdallas.edu>
2018-12-04 00:23:11 -0600

    Added support for remote filenames

--- pysat/instruments/madrigal_methods.py
+++ pysat/instruments/madrigal_methods.py
@@ -196,6 +196,7 @@ def download(date_array, inst_code=None, kindat=None, data_path=None, user=None,
     
     The affiliation field is set to pysat to enable tracking of pysat downloads.
     
+    
     """
   
     import subprocess
@@ -231,6 +232,108 @@ def download(date_array, inst_code=None, kindat=None, data_path=None, user=None,
     except OSError as err:
         print("problem running globalDownload.py, check python path")
 
+
+def list_remote_files(tag, sat_id, inst_code=None, user=None,
+             password=None, supported_tags=None, 
+             url="http://cedar.openmadrigal.org",
+             two_digit_year_break=None):
+    """Lists files available from Madrigal.
+    
+    Parameters
+    ----------
+    tag : (string or NoneType)
+        Denotes type of file to load.  Accepted types are <tag strings>. (default=None)
+    sat_id : (string or NoneType)
+        Specifies the satellite ID for a constellation.  Not used.
+        (default=None)
+    inst_code : string (None)
+        Madrigal instrument code(s), cast as a string.  If multiple are used,
+        separate them with commas.
+    data_path : string (None)
+        Path to directory to download data to.
+    user : string (None)
+        User string input used for download. Provided by user and passed via
+        pysat. If an account
+        is required for dowloads this routine here must error if user not
+        supplied.
+    password : string (None)
+        Password for data download.
+    supported_tags : (dict or NoneType)
+        keys are sat_id, each containing a dict keyed by tag
+        where the values file format template strings. (default=None)
+    url : string ('http://cedar.openmadrigal.org')
+        URL for Madrigal site
+    two_digit_year_break : int
+        If filenames only store two digits for the year, then
+        '1900' will be added for years >= two_digit_year_break
+        and '2000' will be added for years < two_digit_year_break.
+
+    Returns
+    --------
+    Void : (NoneType)
+        Downloads data to disk.
+    
+    Notes
+    -----
+    The user's names should be provided in field user. Ruby Payne-Scott should
+    be entered as Ruby+Payne-Scott
+    
+    The password field should be the user's email address. These parameters
+    are passed to Madrigal when downloading.
+    
+    The affiliation field is set to pysat to enable tracking of pysat downloads.
+
+    Examples
+    --------
+    This method is intended to be set in an instrument support file at the
+    top level using functools.partial
+    ::
+        list_remote_files = functools.partial(mad_meth.list_remote_files, 
+                                              supported_tags=supported_tags,
+                                              inst_code=madrigal_inst_code)
+    
+    """
+  
+    import madrigalWeb.madrigalWeb
+
+    if inst_code is None:
+        raise ValueError("Must supply Madrigal instrument code")
+
+    # currently passes things along if no user and password supplied
+    # need to do this for testing
+    # TODO, implement user and password values in test code
+    # specific to each instrument
+    if user is None:
+        print ('No user information supplied for download.')
+        user = 'pysat_testing'
+    if password is None:
+        print ('Please provide email address in password field.')
+        password = 'pysat_testing@not_real_email.org'
+
+    try:
+        format_str = supported_tags[sat_id][tag]
+    except KeyError:
+        raise ValueError('Problem parsing supported_tags')
+
+    # open connection to Madrigal
+    web_data = madrigalWeb.madrigalWeb.MadrigalData(url)
+    # get list of experiments for instrument from 1900 till now
+    now = pysat.datetime.now()
+    expList = web_data.getExperiments(inst_code,1900,1,1,0,0,0,now.year,
+                                        now.month,now.day,23,59,59)
+    # iterate over experiments to grab files for each one
+    files = []
+    print ("Grabbing filenames for each experiment")
+    print ("A total of", len(expList), "experiments were found")
+    for exp in expList:
+        file_list = web_data.getExperimentFiles(exp.id)
+        files.extend(file_list)
+    # parse these filenames to grab out the ones we want
+    print ("Parsing filenames")
+    stored = pysat._files.parse_fixed_width_filenames(files, format_str)    
+    # process the parsed filenames and return a properly formatted Series
+    print ("Processing filenames")
+    return pysat._files.process_parsed_filenames(stored, two_digit_year_break)              
 
 def filter_data_single_date(self):
     """Filters data to a single date.
