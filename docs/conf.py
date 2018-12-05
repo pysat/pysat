88efd8569c6faff57d9c95e7209ea6653fb656d7
Russell Stoneback <rstoneba@utdallas.edu>
2018-12-04 21:02:14 -0600

    Fix for issue #119
    Removes mock for xarray. xarray now builds on RTD. The true fix was the modifications to the setup enforcing numpy and pandas versions that worked for xarray.

--- docs/conf.py
+++ docs/conf.py
@@ -16,17 +16,6 @@ import sys
 import os
 import shlex
 
-# mock xarray library so it compiles on read the docs
-from unittest.mock import MagicMock
-
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-        return MagicMock()
-
-MOCK_MODULES = ['xarray', 'xarray.Dataset']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
-
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
