35f418ccec1a5829c66e8bb91c10748e2b9afb28
Russell Stoneback <rstoneba@utdallas.edu>
2018-12-04 20:56:21 -0600

    xarray requires numpy>=1.12

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open(os.path.join(here, version_filename)) as version_file:
 
 # change setup.py for readthedocs
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
-install_requires=['xarray', 'pandas>=0.19.2', 'numpy',  'sgp4', 'pyEphem', 'requests',     
+install_requires=['xarray', 'pandas>=0.19.2', 'numpy>=1.12',  'sgp4', 'pyEphem', 'requests',     
                   'pysatCDF', 'apexpy', 'aacgmv2', 'pysatMagVect', 
                   'madrigalWeb', 'h5py', 'PyForecastTools']
 
