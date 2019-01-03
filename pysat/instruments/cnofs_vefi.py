0ba392deb3e8094ddec1e490b3e74374d1737324
rstoneback <rstoneba@utdallas.edu>
2019-01-03 09:08:53 -0600

    Fixed spacing

--- pysat/instruments/cnofs_vefi.py
+++ pysat/instruments/cnofs_vefi.py
@@ -60,7 +60,7 @@ supported_tags = {'':{'dc_b':basic_tag}}
 download = functools.partial(cdw.download, supported_tags)
 # support listing files currently on CDAWeb
 list_remote_files = functools.partial(cdw.list_remote_files, 
-                               supported_tags=supported_tags)
+                                      supported_tags=supported_tags)
 
                     
 def clean(inst):
