795c17873ccd2822865b8665dc6459cb9f654ad7
Angeline G. Burrell <angeline.burrell@nrl.navy.mil>
2019-01-24 13:45:56 -0500

    wrote combine f107 routine
    Added a routine to combine measured and forecasted f107 values

--- pysat/instruments/sw_methods.py
+++ pysat/instruments/sw_methods.py
@@ -225,3 +225,173 @@ def combine_kp(standard_inst=None, recent_inst=None, forecast_inst=None,
     kp_inst.meta.__setitem__('Kp', {kp_inst.meta.notes_label: notes})
 
     return kp_inst
+
+
+def combine_f107(standard_inst, forecast_inst, start=None, stop=None):
+    """ Combine the output from the measured and forecasted F10.7 sources
+
+    Parameters
+    ----------
+    standard_inst : (pysat.Instrument or NoneType)
+        Instrument object containing data for the 'sw' platform, 'f107' name,
+        and '' or 'all' tag
+    forecast_inst : (pysat.Instrument or NoneType)
+        Instrument object containing data for the 'sw' platform, 'f107' name,
+        and '45day' or 'forecast' tag
+    start : (dt.datetime or NoneType)
+        Starting time for combining data, or None to use earliest loaded
+        date from the pysat Instruments (default=None)
+    stop : (dt.datetime)
+        Ending time for combining data, or None to use the latest loaded date
+        from the pysat Instruments (default=None)
+
+    Returns
+    -------
+    f107_inst : (pysat.Instrument)
+        Instrument object containing F10.7 observations for the desired period
+        of time, merging the standard, 45day, and forecasted values based on
+        their reliability
+
+    Notes
+    -----
+    Merging prioritizes the standard data, then the 45day data, and finally
+    the forecast data
+
+    Will not attempt to download any missing data, but will load data
+
+    """
+    # Initialize metadata and flags
+    notes = "Combines data from"
+    stag = standard_inst.tag if len(standard_inst.tag) > 0 else 'daily'
+    tag = 'combined_{:s}_{:s}'.format(stag, forecast_inst.tag)
+    inst_flag = 'standard'
+
+    # If the start or stop times are not defined, get them from the Instruments
+    if start is None:
+        stimes = [inst.index.min() for inst in [standard_inst, forecast_inst]
+                  if len(inst.index) > 0]
+        start = min(stimes) if len(stimes) > 0 else None
+
+    if stop is None:
+        stimes = [inst.index.max() for inst in [standard_inst, forecast_inst]
+                  if len(inst.index) > 0]
+        stop = max(stimes) if len(stimes) > 0 else None
+
+    if start is None or stop is None:
+        raise ValueError("must either load in Instrument objects or provide" +
+                         " starting and ending times")
+
+    # Initialize the output instrument
+    f107_inst = pysat.Instrument()
+    f107_inst.platform = standard_inst.platform
+    f107_inst.name = standard_inst.name
+    f107_inst.tag = tag
+    f107_inst.date = start
+    f107_inst.doy = int(start.strftime("%j"))
+    fill_val = None
+
+    f107_times = list()
+    f107_values = list()
+
+    # Cycle through the desired time range
+    itime = start
+    while itime < stop and inst_flag is not None:
+        # Load and save the standard data for as many times as possible
+        if inst_flag == 'standard':
+            standard_inst.load(date=itime)
+            good_times = standard_inst.index >= itime
+
+            if notes.find("standard") < 0:
+                notes += " the {:} source ({:} to ".format(inst_flag,
+                                                           itime.date())
+
+            if np.any(good_times):
+                if fill_val is None:
+                    f107_inst.meta = standard_inst.meta
+                    fill_val = f107_inst.meta['f107'].fill
+                
+                good_vals = standard_inst['f107'][good_times] != fill_val
+                f107_times.extend(list(standard_inst.index[good_times][good_vals]))
+                f107_values.extend(list(standard_inst['f107'][good_times][good_vals]))
+                itime = f107_times[-1] + pds.DateOffset(days=1)
+            else:
+                inst_flag = 'forecast'
+                notes += "{:})".format(itime.date())
+
+        # Load and save the forecast data for as many times as possible
+        if inst_flag == "forecast":
+            # Determine which files should be loaded
+            if len(forecast_inst.index) == 0:
+                files = np.unique(forecast_inst.files.files[itime:stop])
+            else:
+                files = [None] # No load needed, if already initialized
+
+            # Cycle through all possible files of interest, saving relevant data
+            for filename in files:
+                if filename is not None:
+                    forecast_inst.load(fname=filename)
+
+                if notes.find("forecast") < 0:
+                    notes += " the {:} source ({:} to ".format(inst_flag,
+                                                               itime.date())
+
+                # Check in case there was a problem with the standard data
+                if fill_val is None:
+                    f107_inst.meta = forecast_inst.meta
+                    fill_val = f107_inst.meta['f107'].fill
+
+                # Determine which times to save
+                good_times = forecast_inst.index >= itime
+                good_vals = forecast_inst['f107'][good_times] != fill_val
+
+                # Save desired data and cycle time
+                f107_times.extend(list(forecast_inst.index[good_times][good_vals]))
+                f107_values.extend(list(forecast_inst['f107'][good_times][good_vals]))
+                itime = f107_times[-1] + pds.DateOffset(days=1)
+            notes += "{:})".format(itime.date())
+
+            inst_flag = None
+
+    if inst_flag is not None:
+        notes += "{:})".format(itime.date())
+
+    # Determine if the beginning or end of the time series needs to be padded
+    del_time = (np.array(f107_times[1:]) - np.array(f107_times[:-1])).min()
+    freq = "{:.0f}S".format(del_time.total_seconds())
+    date_range = pds.date_range(start=start, end=stop, freq=freq)
+
+    if date_range[0] < f107_times[0]:
+        # Extend the time and value arrays from their beginning with fill values
+        itime = abs(date_range - f107_times[0]).argmin()
+        f107_times.reverse()
+        f107_values.reverse()
+        extend_times = list(date_range[:itime])
+        extend_times.reverse()
+        f107_times.extend(extend_times)
+        f107_values.extend([fill_val for kk in extend_times])
+        f107_times.reverse()
+        f107_values.reverse()
+
+    if date_range[-1] > f107_times[-1]:
+        # Extend the time and value arrays from their end with fill values
+        itime = abs(date_range - f107_times[-1]).argmin() + 1
+        extend_times = list(date_range[itime:])
+        f107_times.extend(extend_times)
+        f107_values.extend([fill_val for kk in extend_times])
+
+    # Save output data
+    f107_inst.data = pds.DataFrame(f107_values, columns=['f107'], index=f107_times)
+
+    # Resample the output data, filling missing values
+    if(date_range.shape != f107_inst.index.shape or
+       abs(date_range - f107_inst.index).max().total_seconds() > 0.0):
+        f107_inst.data = f107_inst.data.resample(freq).fillna(method=None)
+        if np.isfinite(fill_val):
+            f107_inst.data[np.isnan(f107_inst.data)] = fill_val
+
+    # Update the metadata notes for this custom procedure
+    notes += ", in that order"
+    f107_inst.meta.__setitem__('f107', {f107_inst.meta.notes_label: notes})
+
+    return f107_inst
+
