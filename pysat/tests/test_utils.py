24e0e25ec86b069b1b44f59f8de9314d0a404abc
Angeline G. Burrell <angeline.burrell@nrl.navy.mil>
2019-02-06 11:03:27 -0500

    Update test_utils.py
    Fixed bug in frequency tests

--- pysat/tests/test_utils.py
+++ pysat/tests/test_utils.py
@@ -545,7 +545,7 @@ class TestBasics():
 
         self.testInst.index.freq = pysat.utils.calc_freq(self.testInst.index)
 
-        assert set.testInst.index.freq.find("1S") == 0
+        assert self.testInst.index.freq.find("1S") == 0
 
     def test_calc_freq_ns(self):
         """Test index frequency calculation with nanosecond output"""
@@ -555,7 +555,7 @@ class TestBasics():
                                                  uts=np.arange(0.0, 0.04, .01))
         freq = pysat.utils.calc_freq(tind)
 
-        assert set.testInst.index.freq.find("10000000N") == 0
+        assert self.testInst.index.freq.find("10000000N") == 0
 
     def test_calc_freq_len_fail(self):
         """Test index frequency calculation with empty list"""
