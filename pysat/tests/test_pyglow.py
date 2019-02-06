07b3438f9730079dcca87813e5bede9403f28802
Jeff Klenzing <jklenzing@gmail.com>
2019-02-06 09:21:56 -0500

    simple test

---
+++ pysat/tests/test_pyglow.py
@@ -0,0 +1,13 @@
+"""
+tests the pysat - pyglow operations
+"""
+import pysat
+
+from nose.tools import assert_raises, raises
+import nose.tools
+
+
+def test_import_pyglow(self):
+    """Can we load pyglow?"""
+
+    import pyglow
