60a01d68fdb78550f8ceadf06328a74b1323d297
Jeff Klenzing <jklenzing@gmail.com>
2019-02-09 22:51:29 -0500

    added unit test and updated style

--- pysat/tests/test_coords.py
+++ pysat/tests/test_coords.py
@@ -69,10 +69,8 @@ def test_geodetic_to_geocentric_inverse():
     lat3, lon3, rad_e = pysat.coords.geodetic_to_geocentric(lat2,
                                                             lon_in=lon2,
                                                             inverse=True)
-    tol = 1e-9
-
-    assert ((np.abs(lon1-lon3) < np.abs(lon1*tol)) &
-            (np.abs(lat1-lat3) < np.abs(lat1*tol)))
+    assert (abs(lon1-lon3) < 1.0e-6)
+    assert (abs(lat1-lat3) < 1.0e-6)
 
 
 def test_geodetic_to_geocentric_horizontal_inverse():
@@ -91,12 +89,10 @@ def test_geodetic_to_geocentric_horizontal_inverse():
         pysat.coords.geodetic_to_geocentric_horizontal(lat2, lon2, az2, el2,
                                                        inverse=True)
 
-    tol = 1e-9
-
-    assert ((np.abs(lon1-lon3) < np.abs(lon1*tol)) &
-            (np.abs(lat1-lat3) < np.abs(lat1*tol)) &
-            (np.abs(az1-az3) < np.abs(az1*tol)) &
-            (np.abs(el1-el3) < np.abs(el1*tol)))
+    assert (abs(lon1-lon3) < 1.0e-6)
+    assert (abs(lat1-lat3) < 1.0e-6)
+    assert (abs(az1-az3) < 1.0e-6)
+    assert (abs(el1-el3) < 1.0e-6)
 
 
 def test_spherical_to_cartesian_inverse():
@@ -108,11 +104,9 @@ def test_spherical_to_cartesian_inverse():
     az, el, r = pysat.coords.spherical_to_cartesian(x1, y1, z1, inverse=True)
     x2, y2, z2 = pysat.coords.spherical_to_cartesian(az, el, r, inverse=False)
 
-    tol = 1e-9
-
-    assert ((np.abs(x1-x2) < np.abs(x1*tol)) &
-            (np.abs(y1-y2) < np.abs(y1*tol)) &
-            (np.abs(z1-z2) < np.abs(z1*tol)))
+    assert (abs(x1-x2) < 1.0e-6)
+    assert (abs(y1-y2) < 1.0e-6)
+    assert (abs(z1-z2) < 1.0e-6)
 
 
 def test_global_to_local_cartesian_inverse():
@@ -130,8 +124,25 @@ def test_global_to_local_cartesian_inverse():
     x3, y3, z3 = pysat.coords.global_to_local_cartesian(x2, y2, z2,
                                                         lat, lon, rad,
                                                         inverse=True)
-    tol = 1e-9
+    assert (abs(x1-x3) < 1.0e-6)
+    assert (abs(y1-y3) < 1.0e-6)
+    assert (abs(z1-z3) < 1.0e-6)
 
-    assert ((np.abs(x1-x3) < np.abs(x1*tol)) &
-            (np.abs(y1-y3) < np.abs(y1*tol)) &
-            (np.abs(z1-z3) < np.abs(z1*tol)))
+
+def test_local_horizontal_to_global_geo():
+    """Tests the conversion of the local horizontal to global geo"""
+
+    az = 30.0
+    el = 45.0
+    dist = 1000.0
+    lat0 = 45.0
+    lon0 = 0.0
+    alt0 = 400.0
+
+    latx, lonx, radx = \
+        pysat.coords.local_horizontal_to_global_geo(az, el, dist,
+                                                    lat0, lon0, alt0)
+
+    assert (abs(latx - 50.419037572472625) < 1.0e-6)
+    assert (abs(lonx + 7.694008395350697) < 1.0e-6)
+    assert (abs(radx - 7172.15486518744) < 1.0e-6)
