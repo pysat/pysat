489fc22101ed11027a5734fce05483276ec8b79b
Jeff Klenzing <jklenzing@gmail.com>
2019-02-13 08:14:42 -0500

    init

--- pysat/__init__.py
+++ pysat/__init__.py
@@ -71,7 +71,7 @@ if data_dir == '':
           ' to top-level directory that will/does contain science data.')))
 
 from pandas import Panel, DataFrame, Series, datetime
-from . import model_utils, utils
+from . import utils, model_utils
 from ._constellation import Constellation
 from ._instrument import Instrument
 from ._meta import Meta
