8ca094fbf4d1bb38eb8d742531fcb0629e60d4b5
Jeff Klenzing <jklenzing@gmail.com>
2019-03-02 14:22:46 -0500

    added de2_nacs

--- pysat/instruments/__init__.py
+++ pysat/instruments/__init__.py
@@ -7,11 +7,11 @@ is contained within a subpackage of this set.
 """
 
 __all__ = ['champ_star', 'cnofs_ivm', 'cnofs_plp', 'cnofs_vefi', 'cosmic_gps',
-           'cosmic2013_gps', 'de2_idm', 'de2_lang', 'de2_idm', 'de2_wats',
-           'demeter_iap', 'dmsp_ivm', 'icon_ivm', 'icon_euv', 'icon_fuv',
-           'icon_mighti', 'iss_fpmu',  'jro_isr', 'omni_hro', 'pysat_sgp4',
-           'rocsat1_ivm', 'sport_ivm', 'superdarn_grdex',
-           'supermag_magnetometer', 'sw_dst', 'sw_kp', 'sw_f107', 'timed_see',
-           'ucar_tiegcm', ]
+           'cosmic2013_gps', 'de2_idm', 'de2_lang', 'de2_idm', 'de2_nacs',
+           'de2_wats', 'demeter_iap', 'dmsp_ivm',
+           'icon_ivm', 'icon_euv', 'icon_fuv', 'icon_mighti', 'iss_fpmu',
+           'jro_isr', 'omni_hro', 'pysat_sgp4', 'rocsat1_ivm', 'sport_ivm',
+           'superdarn_grdex', 'supermag_magnetometer', 'sw_dst', 'sw_kp',
+           'sw_f107', 'timed_see', 'ucar_tiegcm', ]
 
 from . import *
