cfc0f81903e5ba719762dbaed820d31f9c13319f
Jeff Klenzing <jklenzing@gmail.com>
2019-03-04 08:25:58 -0500

    basic test file

---
+++ pysat/tests/test_ssnl_plot.py
@@ -0,0 +1,11 @@
+"""
+tests the pysat averaging code
+"""
+import numpy as np
+import sys
+
+from nose.tools import assert_raises, raises
+import pandas as pds
+
+import pysat
+from pysat.ssnl import plot
