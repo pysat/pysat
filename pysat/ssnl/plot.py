000885ad12b74214b8d7cab863ce181486c9f56f
Jeff Klenzing <jklenzing@gmail.com>
2019-03-04 12:36:41 -0500

    updates to plot routine

--- pysat/ssnl/plot.py
+++ pysat/ssnl/plot.py
@@ -1,9 +1,9 @@
 from __future__ import print_function
 from __future__ import absolute_import
 
-from matplotlib import cm
 import matplotlib.colors as colors
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 
@@ -38,38 +38,23 @@ def scatterplot(inst, labelx, labely, data_label, datalim, xlim=None,
     figs = []
     axs = []
 
-    # check if doing multiple data quantities
+    # Check for list-like behaviour of data_label
     if type(data_label) is str:
-        # max of one data type
+        data_label = [data_label]
+
+    # multiple data to be plotted
+    for i in np.arange(len(data_label)):
         figs.append(plt.figure())
-        ax1 = figs[0].add_subplot(211, projection='3d')
-        ax2 = figs[0].add_subplot(212)
+        ax1 = figs[i].add_subplot(211, projection='3d')
+        ax2 = figs[i].add_subplot(212)
         axs.append((ax1, ax2))
-        plt.suptitle(data_label)
+        plt.suptitle(data_label[i])
         if xlim is not None:
             ax1.set_xlim(xlim)
         ax2.set_xlim(xlim)
         if ylim is not None:
             ax1.set_ylim(ylim)
         ax2.set_ylim(ylim)
-        data_label = [data_label]
-        plt.hold(True)
-    else:
-        # multiple data to be plotted
-        for i in np.arange(len(data_label)):
-            figs.append(plt.figure())
-            ax1 = figs[i].add_subplot(211, projection='3d')
-            ax2 = figs[i].add_subplot(212)
-            axs.append((ax1, ax2))
-            # axs.append( figs[i].add_subplot(111, projection='3d') )
-            plt.suptitle(data_label[i])
-            if xlim is not None:
-                ax1.set_xlim(xlim)
-            ax2.set_xlim(xlim)
-            if ylim is not None:
-                ax1.set_ylim(ylim)
-            ax2.set_ylim(ylim)
-            plt.hold(True)
 
     # norm method so that data may be scaled to colors appropriately
     norm = colors.Normalize(vmin=datalim[0], vmax=datalim[1])
@@ -77,17 +62,17 @@ def scatterplot(inst, labelx, labely, data_label, datalim, xlim=None,
     for i, inst in enumerate(inst):
         for j, (fig, ax) in enumerate(zip(figs, axs)):
             if len(inst.data) > 0:
-                if (len(inst.data[labelx]) > 0) & \
-                   (len(inst.data[labely]) > 0) & \
-                   (len(inst.data[data_label[j]]) > 0):
+                check1 = len(inst.data[labelx]) > 0
+                check2 = len(inst.data[labely]) > 0
+                check3 = len(inst.data[data_label[j]]) > 0
+                if (check1 & check2 & check3):
                     p[j] = ax[0].scatter(inst.data[labelx], inst.data[labely],
                                          inst.data[data_label[j]], zdir='z',
-                                         c=inst.data[data_label[j]],
-                                         cmap=cm.jet, norm=norm,
-                                         linewidth='0', edgecolors=None)
+                                         c=inst.data[data_label[j]], norm=norm,
+                                         linewidth=0, edgecolors=None)
                     ax[1].scatter(inst.data[labelx], inst.data[labely],
-                                  c=inst.data[data_label[j]], cmap=cm.jet,
-                                  norm=norm, linewidth=0.00000000001,
+                                  c=inst.data[data_label[j]],
+                                  norm=norm,
                                   alpha=0.5, edgecolor=None)
 
     for j, (fig, ax) in enumerate(zip(figs, axs)):
@@ -96,6 +81,7 @@ def scatterplot(inst, labelx, labely, data_label, datalim, xlim=None,
         except:
             print('Tried colorbar but failed, thus no colorbar.')
         ax[0].elev = 30.
+
     # interactive plotting back on
     plt.ion()
     return figs
--- pysat/tests/test_ssnl_plot.py
+++ pysat/tests/test_ssnl_plot.py
@@ -9,3 +9,23 @@ import pandas as pds
 
 import pysat
 from pysat.ssnl import plot
+
+
+class TestBasics():
+    def setup(self):
+        """Runs before every method to create a clean testing setup."""
+        self.testInst = pysat.Instrument('pysat', 'testing',
+                                         clean_level='clean')
+        self.testInst.bounds = (pysat.datetime(2008, 1, 1),
+                                pysat.datetime(2008, 1, 1))
+
+    def teardown(self):
+        """Runs after every method to clean up previous testing."""
+        del self.testInst
+
+    def test_scatterplot(self):
+        figs = plot.scatterplot(self.testInst, 'longitude', 'latitude',
+                                ['slt'], [0.0, 24.0])
+
+        axes = figs[0].get_axes()
+        assert len(axes) == 3
