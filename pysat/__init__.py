f3128f2bfd21a296837ef820a4dad1bd67030dfe
Jeff Klenzing <jklenzing@gmail.com>
2019-03-06 13:07:37 -0500

    add different directory for travis tests

--- pysat/__init__.py
+++ pysat/__init__.py
@@ -58,10 +58,12 @@ if not os.path.isdir(pysat_dir):
     os.mkdir(pysat_dir)
     print('Created .pysat directory in user home directory to store settings.')
     # create file with default data directory
-    data_dir = os.path.join(home_dir, 'pysatData')
+    if not (os.environ.get('TRAVIS') == 'true'):
+        data_dir = os.path.join(home_dir, 'pysatData')
+    else:
+        data_dir = '/home/travis/build/rstoneback/pysat/pysatData'
     with open(os.path.join(pysat_dir, 'data_path.txt'), 'w') as f:
         f.write(data_dir)
-    os.mkdir(data_dir)
     print(''.join(("\nHi there!  Pysat will nominally store data in the "
                    "'pysatData' directory at the user's home directory level. "
                    "Run pysat.utils.set_data_dir to specify a different "
