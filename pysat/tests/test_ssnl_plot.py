c0b52668f8bea69ddda4ccdb2b28ea4702d21c0a
Jeff Klenzing <jklenzing@gmail.com>
2019-05-08 22:59:52 -0400

    improved unit tests

--- pysat/tests/test_ssnl_plot.py
+++ pysat/tests/test_ssnl_plot.py
@@ -22,17 +22,44 @@ class TestBasics():
         del self.testInst
         plt.close()
 
-    def test_scatterplot(self):
+    def test_scatterplot_w_ioff(self):
         """Check if scatterplot generates"""
 
-        interactive_mode = mpl.is_interactive()
+        plt.ioff()
         figs = plot.scatterplot(self.testInst, 'longitude', 'latitude',
                                 'slt', [0.0, 24.0])
 
         axes = figs[0].get_axes()
         assert len(figs) == 1
         assert len(axes) == 3
-        assert interactive_mode == mpl.is_interactive()
+        assert not mpl.is_interactive()
+
+    def test_scatterplot_w_ion(self):
+        """Check if scatterplot generates and resets to interactive mode"""
+
+        plt.ion()
+        figs = plot.scatterplot(self.testInst, 'longitude', 'latitude',
+                                'slt', [0.0, 24.0])
+
+        axes = figs[0].get_axes()
+        assert len(figs) == 1
+        assert len(axes) == 3
+        assert mpl.is_interactive()
+
+    def test_scatterplot_w_limits(self):
+        """Check if scatterplot generates with appropriate limits"""
+
+        figs = plot.scatterplot(self.testInst, 'longitude', 'latitude',
+                                'slt', [0.0, 24.0],
+                                xlim=[0, 360], ylim=[-80, 80])
+
+        axes = figs[0].get_axes()
+        assert len(figs) == 1
+        assert len(axes) == 3
+        assert axes[0].get_xlim() == (0, 360)
+        assert axes[1].get_xlim() == (0, 360)
+        assert axes[0].get_ylim() == (-80, 80)
+        assert axes[1].get_ylim() == (-80, 80)
 
     def test_multiple_scatterplots(self):
         """Check if multiple scatterplots generate"""
