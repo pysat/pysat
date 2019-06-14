904409d37c8b32370ec4bcd4462380186bf04187
Jeff Klenzing <jklenzing@gmail.com>
2019-06-13 21:36:57 -0400

    spacing

--- pysat/model_utils.py
+++ pysat/model_utils.py
@@ -31,7 +31,7 @@ def satellite_view_through_model(sat, tie, scoords, tlabels):
     tlabels : string or list of strings
         Variable names from model to interpolate onto sat locations
     """
-    
+
     import warnings
 
     warnings.warn('Preliminary code.  Currently tested for TIE-GCM')
@@ -39,26 +39,26 @@ def satellite_view_through_model(sat, tie, scoords, tlabels):
     # tiegcm is in pressure levels, need in altitude, but on regular
     # grid
     import scipy.interpolate as interpolate
-    
+
     # create input array using satellite time/position
     if isinstance(scoords, str):
         scoords = [scoords]
     coords = [sat[coord] for coord in scoords]
     coords.insert(0, sat.index.values.astype(int))
     sat_pts = [inp for inp in zip(*coords)]
-    
+
     interp = {}
     if isinstance(tlabels, str):
         tlabels = [tlabels]
     for label in tlabels:
-        points = [tie.data.coords[dim].values if dim != 'time' else 
-                  tie.data.coords[dim].values.astype(int) 
+        points = [tie.data.coords[dim].values if dim != 'time' else
+                  tie.data.coords[dim].values.astype(int)
                   for dim in tie[label].dims]
-        interp[label] = interpolate.RegularGridInterpolator(points, 
+        interp[label] = interpolate.RegularGridInterpolator(points,
                                                             tie[label].values,
                                                             bounds_error=False,
                                                             fill_value=None)
-        sat[''.join(('model_', label))] = interp[label](sat_pts)    
+        sat[''.join(('model_', label))] = interp[label](sat_pts)
 
 
 def compare_model_and_inst(pairs=None, inst_name=[], mod_name=[],
