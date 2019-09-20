80bcc4ca52b4f144dd1ecef333e2f5724fe504b9
Jeff Klenzing <jklenzing@gmail.com>
2019-09-19 20:04:54 -0400

    BUG: remove manual install in travis

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ from setuptools import setup
 # To use a consistent encoding
 from codecs import open
 import os
+import sys
 
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'description.txt'), encoding='utf-8') as f:
@@ -19,11 +20,19 @@ with open(os.path.join(here, version_filename)) as version_file:
 
 # change setup.py for readthedocs
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
-install_requires = ['xarray', 'pandas>=0.23, <0.25', 'numpy>=1.12',
-                    'sgp4', 'pyEphem', 'requests', 'beautifulsoup4',
-                    'lxml', 'netCDF4', 'matplotlib', 'pysatCDF',
-                    'apexpy', 'aacgmv2', 'pysatMagVect', 'madrigalWeb',
-                    'h5py', 'PyForecastTools', 'pyglow']
+if sys.version_info.major == 2:
+    install_requires = ['xarray<0.12', 'pandas>=0.23, <0.25',
+                        'numpy>=1.12, <1.17', 'scipy<1.3', 'sgp4',
+                        'pyEphem', 'requests', 'beautifulsoup4',
+                        'lxml', 'netCDF4', 'matplotlib<3.1', 'pysatCDF',
+                        'apexpy', 'aacgmv2', 'pysatMagVect', 'madrigalWeb',
+                        'h5py', 'PyForecastTools', 'pyglow']
+else:
+    install_requires = ['xarray', 'pandas>=0.23, <0.25', 'numpy>=1.12',
+                        'sgp4', 'pyEphem', 'requests', 'beautifulsoup4',
+                        'lxml', 'netCDF4', 'matplotlib', 'pysatCDF',
+                        'apexpy', 'aacgmv2', 'pysatMagVect', 'madrigalWeb',
+                        'h5py', 'PyForecastTools', 'pyglow']
 
 # all packages after pysatCDF are excluded if on ReadTheDocs
 if on_rtd:
