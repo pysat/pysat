ceaf775e471838dc7b03e3800d0bdae46e747271
Jeff Klenzing <jklenzing@gmail.com>
2019-09-28 16:28:50 -0400

    TST: add test for deprecation

--- pysat/tests/test_utils.py
+++ pysat/tests/test_utils.py
@@ -39,6 +39,23 @@ def remove_files(inst):
                 os.unlink(file_path)
 
 
+def test_deprecation_warning_computational_form():
+    """Test if computational form in utils is deprecated"""
+
+    import pandas as pds
+    import warnings
+
+    data = pds.Series([0, 1, 2])
+    warnings.simplefilter("always")
+    dslice1 = pysat.ssnl.computational_form(data)
+    with warnings.catch_warnings(record=True) as w:
+        dslice2 = pysat.utils.computational_form(data)
+
+    assert (dslice1 == dslice2).all()
+    assert len(w) == 1
+    assert w[0].category == DeprecationWarning
+
+
 class TestBasics():
     def setup(self):
         """Runs before every method to create a clean testing setup."""
