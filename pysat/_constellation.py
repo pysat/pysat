189ef4607433f6ac1abcbc253c3863e80863c68c
Jeff Klenzing <jklenzing@gmail.com>
2019-12-31 16:16:49 -0500

    BUG: remove pysat.ssnl routine from constellation

--- pysat/_constellation.py
+++ pysat/_constellation.py
@@ -237,9 +237,108 @@ class Constellation(object):
         binx = None
 
         # TODO modify output
-        # TODO replace _calc_2d_median with actual routine
-        out_2d = _calc_2d_median(ans, data_label, binx, biny, xarr, yarr,
-                                 zarr, numx, numy, numz)
+        #  Begin excerpt code from pysatSeasons 0.1.0
+        medianAns = [[[None for i in xarr] for j in yarr] for k in zarr]
+        countAns = [[[None for i in xarr] for j in yarr] for k in zarr]
+        devAns = [[[None for i in xarr] for j in yarr] for k in zarr]
+
+        # all of the loading and storing data is done
+        # determine what kind of data is stored
+        # if just numbers, then use numpy arrays to store data
+        # if the data is a more generalized object, use lists to store data
+        # need to find first bin with data
+        dataType = [None for i in np.arange(numz)]
+        # for each data product label, find the first nonempty bin
+        # and select its type
+        for zk in zarr:
+            breakNow = False
+            for yj in yarr:
+                for xi in xarr:
+                    if len(ans[zk][yj][xi]) > 0:
+                        dataType[zk] = type(ans[zk][yj][xi][0])
+                        breakNow = True
+                        break
+                if breakNow:
+                    break
+
+        # determine if normal number objects are being used or if there
+        # are more complicated objects
+        objArray = [False] * len(zarr)
+        for i, thing in enumerate(dataType):
+            if thing == pds.core.series.Series:
+                objArray[i] = 'S'
+            elif thing == pds.core.frame.DataFrame:
+                objArray[i] = 'F'
+            else:
+                # other, simple scalaRs
+                objArray[i] = 'R'
+
+        objArray = np.array(objArray)
+
+        # if some pandas data series are returned in average, return a list
+        objidx, = np.where(objArray == 'S')
+        if len(objidx) > 0:
+            for zk in zarr[objidx]:
+                for yj in yarr:
+                    for xi in xarr:
+                        if len(ans[zk][yj][xi]) > 0:
+                            ans[zk][yj][xi] = list(ans[zk][yj][xi])
+                            medianAns[zk][yj][xi] = \
+                                pds.DataFrame(ans[zk][yj][xi]).median(axis=0)
+                            countAns[zk][yj][xi] = len(ans[zk][yj][xi])
+                            devAns[zk][yj][xi] = \
+                                pds.DataFrame([abs(temp - medianAns[zk][yj][xi])
+                                               for temp in
+                                               ans[zk][yj][xi]]).median(axis=0)
+
+        # if some pandas DataFrames are returned in average, return a list
+        objidx, = np.where(objArray == 'F')
+        if len(objidx) > 0:
+            for zk in zarr[objidx]:
+                for yj in yarr:
+                    for xi in xarr:
+                        if len(ans[zk][yj][xi]) > 0:
+                            ans[zk][yj][xi] = list(ans[zk][yj][xi])
+                            countAns[zk][yj][xi] = len(ans[zk][yj][xi])
+                            test = pds.Panel.from_dict(dict([(i, temp)
+                                                       for i, temp in
+                                                       enumerate(ans[zk][yj][xi])]))
+                            medianAns[zk][yj][xi] = test.median(axis=0)
+                            devAns[zk][yj][xi] = \
+                                (test.subtract(medianAns[zk][yj][xi],
+                                               axis=0)).abs().median(axis=0,
+                                                                     skipna=True)
+
+        objidx, = np.where(objArray == 'R')
+        if len(objidx) > 0:
+            for zk in zarr[objidx]:
+                medianAns[zk] = np.zeros((numy, numx))*np.nan
+                countAns[zk] = np.zeros((numy, numx))*np.nan
+                devAns[zk] = np.zeros((numy, numx))*np.nan
+                for yj in yarr:
+                    for xi in xarr:
+                        # convert deque storing data into numpy array
+                        ans[zk][yj][xi] = np.array(ans[zk][yj][xi])
+                        # filter out an NaNs in the arrays
+                        idx, = np.where(np.isfinite(ans[zk][yj][xi]))
+                        ans[zk][yj][xi] = (ans[zk][yj][xi])[idx]
+                        # perform median averaging
+                        if len(idx) > 0:
+                            medianAns[zk][yj, xi] = np.median(ans[zk][yj][xi])
+                            countAns[zk][yj, xi] = len(ans[zk][yj][xi])
+                            devAns[zk][yj, xi] = np.median(abs(ans[zk][yj][xi] -
+                                                           medianAns[zk][yj, xi]))
+
+        # prepare output
+        out_2d = {}
+        for i, label in enumerate(data_label):
+            out_2d[label] = {'median': medianAns[i],
+                             'count': countAns[i],
+                             'avg_abs_dev': devAns[i],
+                             'bin_x': binx,
+                             'bin_y': biny}
+
+        #  End of Excerpt code
 
         # Transform output
         output = {}
