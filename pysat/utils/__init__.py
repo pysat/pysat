370e0b5b32cdbb9150322752733ab3a2385a725a
Jeff Klenzing <jklenzing@gmail.com>
2020-02-03 15:19:02 -0500

    STY: absolute import

--- pysat/utils/__init__.py
+++ pysat/utils/__init__.py
@@ -9,5 +9,5 @@ of formats, loading of files, and user-supplied info
 for the pysat data directory structure.
 """
 
-from . import coords, time, registry
-from pysat._core import set_data_dir, scale_units, load_netcdf4
+from pysat.utils import coords, time, registry
+from pysat.utils._core import set_data_dir, scale_units, load_netcdf4
