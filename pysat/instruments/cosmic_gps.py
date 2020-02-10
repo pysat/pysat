08857c12388788216eda9df563dabdad0693340c
rstoneback <rstoneba@utdallas.edu>
2020-02-10 14:15:32 -0600

    BUG: Fixed altitude_bin as valid keyword so that it works

--- pysat/instruments/cosmic_gps.py
+++ pysat/instruments/cosmic_gps.py
@@ -141,7 +141,7 @@ def list_files(tag=None, sat_id=None, data_path=None, format_str=None):
         return pysat.Series(None)
 
 
-def load(fnames, tag=None, sat_id=None):
+def load(fnames, tag=None, sat_id=None, altitude_bin=None):
     """Load COSMIC GPS files.
 
     Parameters
@@ -152,6 +152,9 @@ def load(fnames, tag=None, sat_id=None):
         tag or None (default=None)
     sat_id : (str or NoneType)
         satellite id or None (default=None)
+    altitude_bin : integer
+        Number of kilometers to bin altitude profiles by when loading.
+        Currently only supported for tag='ionprf'.
 
     Returns
     -------
@@ -162,12 +165,18 @@ def load(fnames, tag=None, sat_id=None):
 
     """
 
+    # input check
+    if altitude_bin is not None:
+        if tag != 'ionprf':
+            raise ValueError('altitude_bin only supported for "tag=ionprf"')
+
     num = len(fnames)
     # make sure there are files to read
     if num != 0:
         # call separate load_files routine, segemented for possible
         # multiprocessor load, not included and only benefits about 20%
-        output = pysat.DataFrame(load_files(fnames, tag=tag, sat_id=sat_id))
+        output = pysat.DataFrame(load_files(fnames, tag=tag, sat_id=sat_id,
+                                            altitude_bin=altitude_bin))
         utsec = output.hour * 3600. + output.minute * 60. + output.second
         output.index = \
             pysat.utils.time.create_datetime_index(year=output.year,
