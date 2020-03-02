74e5b11c4ed24dbfc45010d29717e016b3a827a0
Jeff Klenzing <jklenzing@gmail.com>
2020-03-02 08:37:11 -0500

    TST: limit values in ssnl tests

--- pysat/tests/test_ssnl_ave.py
+++ pysat/tests/test_ssnl_ave.py
@@ -174,8 +174,8 @@ class TestDeprecation():
 class TestFrameProfileAverages():
     def setup(self):
         """Runs before every method to create a clean testing setup."""
-        self.testInst = pysat.Instrument('pysat', 'testing2D',
-                                         clean_level='clean')
+        self.testInst = pysat.Instrument(platform='pysat', name='testing2D',
+                                         sat_id='500', clean_level='clean')
         self.testInst.bounds = (pysat.datetime(2008, 1, 1),
                                 pysat.datetime(2008, 1, 3))
         self.dname = 'alt_profiles'
@@ -224,8 +224,8 @@ class TestFrameProfileAverages():
 class TestSeriesProfileAverages():
     def setup(self):
         """Runs before every method to create a clean testing setup."""
-        self.testInst = pysat.Instrument('pysat', 'testing2D',
-                                         clean_level='clean')
+        self.testInst = pysat.Instrument(platform='pysat', name='testing2D',
+                                         sat_id='500', clean_level='clean')
         self.testInst.bounds = (pysat.datetime(2008, 1, 1),
                                 pysat.datetime(2008, 2, 1))
         self.dname = 'series_profiles'
@@ -397,8 +397,8 @@ class TestHeterogenousConstellation:
 class Test2DConstellation:
     def setup(self):
         insts = []
-        insts.append(pysat.Instrument('pysat', 'testing2d',
-                     clean_level='clean'))
+        insts.append(pysat.Instrument(platform='pysat', name='testing2D',
+                                      sat_id='500', clean_level='clean'))
         self.testC = pysat.Constellation(insts)
         self.bounds = (pysat.datetime(2008, 1, 1), pysat.datetime(2008, 1, 3))
 
