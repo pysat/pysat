3e6d383356d9b47be42b863a5b0a19be03ccb649
Jeff Klenzing <jklenzing@gmail.com>
2020-04-23 23:53:52 -0400

    ENH: remove Madrigal instruments

--- pysat/instruments/__init__.py
+++ pysat/instruments/__init__.py
@@ -8,9 +8,9 @@ is contained within a subpackage of this set.
 
 __all__ = ['champ_star', 'cnofs_ivm', 'cnofs_plp', 'cnofs_vefi', 'cosmic_gps',
            'de2_lang', 'de2_nacs', 'de2_rpa', 'de2_wats',
-           'demeter_iap', 'dmsp_ivm',
+           'demeter_iap',
            'icon_ivm', 'icon_euv', 'icon_fuv', 'icon_mighti',
-           'iss_fpmu',  'jro_isr', 'omni_hro', 'rocsat1_ivm',
+           'iss_fpmu',  'omni_hro', 'rocsat1_ivm',
            'sport_ivm', 'superdarn_grdex', 'supermag_magnetometer',
            'sw_dst', 'sw_kp', 'sw_f107', 'timed_saber', 'timed_see',
            'pysat_testing', 'pysat_testing_xarray', 'pysat_testing2d',
