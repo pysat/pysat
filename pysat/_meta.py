1349d0f2590fd1463aac1a985938c55bc7d7686e
rstoneback <rstoneba@utdallas.edu>
2020-06-23 11:45:30 -0500

    STY: Typo and formatting improvement

--- pysat/_meta.py
+++ pysat/_meta.py
@@ -82,7 +82,8 @@ class Meta(object):
         String used to label fill value in storage. Defaults to 'fill' per
         netCDF4 standard
     export_nan: list
-        List of labels that should be exported even if their value is nan. By default, metadate with a value of nan will be exluded from export.
+        List of labels that should be exported even if their value is nan.
+        By default, metadata with a value of nan will be exluded from export.
 
 
     Notes
@@ -180,7 +181,7 @@ class Meta(object):
                  scale_label='scale', min_label='value_min',
                  max_label='value_max', fill_label='fill',
                  export_nan=[]):
-        
+
         # set mutability of Meta attributes
         self.mutable = True
 
@@ -401,7 +402,7 @@ class Meta(object):
 
 
     def __setattr__(self, name, value):
-        """Conditionally sets attributes based on self.mutable flag 
+        """Conditionally sets attributes based on self.mutable flag
         @properties are assumed to be mutable.
         We avoid recursively setting properties using
         method from https://stackoverflow.com/a/15751135
