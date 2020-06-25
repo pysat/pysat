e8f703ef6a8f40512e79a5e237f7b5b263ea75bd
Jeff Klenzing <jklenzing@gmail.com>
2020-06-25 19:19:42 -0400

    TST: move remove_leading_text tests to general methods

--- pysat/tests/test_methods_general.py
+++ pysat/tests/test_methods_general.py
@@ -1,5 +1,6 @@
 import pytest
 
+import pysat
 from pysat.instruments.methods import general as gen
 
 
@@ -25,3 +26,51 @@ class TestGenMethods():
         with pytest.raises(ValueError) as excinfo:
             gen.list_files(**self.kwargs)
         assert str(excinfo.value).find(err_msg) >= 0
+
+
+class TestICONIVMCustom():
+    def setup(self):
+        """Runs before every method to create a clean testing setup."""
+        # Load a test instrument
+        self.testInst = pysat.Instrument('pysat', 'testing', tag='12',
+                                         clean_level='clean')
+        self.testInst.load(2009, 1)
+
+    def teardown(self):
+        """Runs after every method to clean up previous testing."""
+        del self.testInst
+
+    def test_remove_names_wo_target(self):
+        self.testInst.tag = ''
+        self.testInst.sat_id = 'a'
+
+        self.testInst['ICON_L27_Blurp'] = self.testInst['dummy1']
+        gen.remove_leading_text(self.testInst)
+        # check variables unchanged
+        assert (len(self.testInst['ICON_L27_Blurp']) > 0)
+        # check other names untouched
+        assert (len(self.testInst['dummy1']) > 0)
+
+    def test_remove_names_target(self):
+        self.testInst.tag = ''
+        self.testInst.sat_id = 'a'
+
+        self.testInst['ICON_L27_Blurp'] = self.testInst['dummy1']
+        gen.remove_leading_text(self.testInst, target='ICON_L27')
+        # check prepended text removed
+        assert (len(self.testInst['_Blurp']) > 0)
+        # check other names untouched
+        assert (len(self.testInst['dummy1']) > 0)
+
+
+class TestRemoveLeadTextXarray(TestICONIVMCustom):
+    def setup(self):
+        """Runs before every method to create a clean testing setup."""
+        # Load a test instrument
+        self.testInst = pysat.Instrument('pysat', 'testing_xarray', tag='12',
+                                         clean_level='clean')
+        self.testInst.load(2009, 1)
+
+    def teardown(self):
+        """Runs after every method to clean up previous testing."""
+        del self.testInst
