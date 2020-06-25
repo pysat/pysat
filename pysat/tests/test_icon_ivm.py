ecfbfb24b9f87ffbec878c05cb5bb7c8e52f3b56
Jeff Klenzing <jklenzing@gmail.com>
2020-06-25 18:56:22 -0400

    TST: update tests for remove_leading_text

--- pysat/tests/test_icon_ivm.py
+++ pysat/tests/test_icon_ivm.py
@@ -1,5 +1,5 @@
 import pysat
-import pysat.instruments.icon_ivm as icivm
+import pysat.instruments.methods.general as icivm
 
 
 class TestICONIVMCustom():
@@ -14,14 +14,14 @@ class TestICONIVMCustom():
         """Runs after every method to clean up previous testing."""
         del self.testInst
 
-    def test_remove_names_tag_level(self):
+    def test_remove_names_wo_target(self):
         self.testInst.tag = ''
         self.testInst.sat_id = 'a'
 
         self.testInst['ICON_L27_Blurp'] = self.testInst['dummy1']
-        icivm.remove_icon_names(self.testInst)
-        # check prepended text removed
-        assert (len(self.testInst['Blurp']) > 0)
+        icivm.remove_leading_text(self.testInst)
+        # check variables unchanged
+        assert (len(self.testInst['ICON_L27_Blurp']) > 0)
         # check other names untouched
         assert (len(self.testInst['dummy1']) > 0)
 
@@ -30,8 +30,21 @@ class TestICONIVMCustom():
         self.testInst.sat_id = 'a'
 
         self.testInst['ICON_L27_Blurp'] = self.testInst['dummy1']
-        icivm.remove_icon_names(self.testInst, target='ICON_L27')
+        icivm.remove_leading_text(self.testInst, target='ICON_L27')
         # check prepended text removed
         assert (len(self.testInst['_Blurp']) > 0)
         # check other names untouched
         assert (len(self.testInst['dummy1']) > 0)
+
+
+class TestRemoveLeadTextXarray(TestICONIVMCustom):
+    def setup(self):
+        """Runs before every method to create a clean testing setup."""
+        # Load a test instrument
+        self.testInst = pysat.Instrument('pysat', 'testing_xarray', tag='12',
+                                         clean_level='clean')
+        self.testInst.load(2009, 1)
+
+    def teardown(self):
+        """Runs after every method to clean up previous testing."""
+        del self.testInst
