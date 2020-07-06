81d887597bc7476ef99abdf45193feb0117b32a6
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2020-07-06 10:44:31 -0400

    STY: testing variable names
    Variable names starting with `test` can be mistakenly gathered as unit test,
    renamed all variables that had this problem.

--- pysat/tests/test_utils_coords.py
+++ pysat/tests/test_utils_coords.py
@@ -12,40 +12,40 @@ from pysat.utils import coords
 class TestCyclicData():
     def setup(self):
         """Runs before every method to create a clean testing setup."""
-        self.test_angles = np.array([340.0, 348.0, 358.9, 0.5, 5.0, 9.87])
+        self.ref_angles = np.array([340.0, 348.0, 358.9, 0.5, 5.0, 9.87])
 
     def teardown(self):
         """Runs after every method to clean up previous testing."""
-        del self.test_angles
+        del self.ref_angles
 
     def test_adjust_cyclic_data_default(self):
         """ Test adjust_cyclic_data with default range """
 
-        test_in = np.radians(self.test_angles) - np.pi
-        test_angles = coords.adjust_cyclic_data(test_in)
+        ref_rad = np.radians(self.ref_angles) - np.pi
+        ref_angles = coords.adjust_cyclic_data(ref_rad)
 
-        assert test_angles.max() < 2.0 * np.pi
-        assert test_angles.min() >= 0.0
+        assert ref_angles.max() < 2.0 * np.pi
+        assert ref_angles.min() >= 0.0
 
     def test_adjust_cyclic_data_custom(self):
         """ Test adjust_cyclic_data with a custom range """
 
-        test_angles = coords.adjust_cyclic_data(self.test_angles,
-                                                high=180.0, low=-180.0)
+        ref_angles = coords.adjust_cyclic_data(self.ref_angles,
+                                               high=180.0, low=-180.0)
 
-        assert test_angles.max() < 180.0
-        assert test_angles.min() >= -180.0
+        assert ref_angles.max() < 180.0
+        assert ref_angles.min() >= -180.0
 
 
 class TestLonSLT():
     def setup(self):
         """Runs after every method to clean up previous testing."""
-        self.test_inst = None
-        self.test_time = dt.datetime(2009, 1, 1)
+        self.py_inst = None
+        self.inst_time = dt.datetime(2009, 1, 1)
 
     def teardown(self):
         """Runs after every method to clean up previous testing."""
-        del self.test_inst, self.test_time
+        del self.py_inst, self.inst_time
 
     #####################################
     # Update Longitude
@@ -53,27 +53,27 @@ class TestLonSLT():
     def test_update_longitude(self, name):
         """Test update_longitude """
 
-        self.test_inst = pysat.Instrument(platform='pysat', name=name)
-        self.test_inst.load(date=self.test_time)
+        self.py_inst = pysat.Instrument(platform='pysat', name=name)
+        self.py_inst.load(date=self.inst_time)
 
         # Test instruments initially define longitude between 0-360 deg
-        assert np.all(self.test_inst.data['longitude'] < 360.0)
-        assert np.all(self.test_inst.data['longitude'] >= 0.0)
+        assert np.all(self.py_inst.data['longitude'] < 360.0)
+        assert np.all(self.py_inst.data['longitude'] >= 0.0)
 
         # Longitude defaults to updating range from -180 to 180 deg
-        coords.update_longitude(self.test_inst, lon_name="longitude")
+        coords.update_longitude(self.py_inst, lon_name="longitude")
 
-        assert np.all(self.test_inst.data['longitude'] < 180.0)
-        assert np.all(self.test_inst.data['longitude'] >= -180.0)
+        assert np.all(self.py_inst.data['longitude'] < 180.0)
+        assert np.all(self.py_inst.data['longitude'] >= -180.0)
 
     def test_bad_lon_name_update_longitude(self):
         """Test update_longitude with a bad longitude name"""
 
-        self.test_inst = pysat.Instrument(platform='pysat', name="testing")
-        self.test_inst.load(date=self.test_time)
+        self.py_inst = pysat.Instrument(platform='pysat', name="testing")
+        self.py_inst.load(date=self.inst_time)
 
         with pytest.raises(ValueError):
-            coords.update_longitude(self.test_inst, lon_name="not longitude")
+            coords.update_longitude(self.py_inst, lon_name="not longitude")
 
     #########################
     # calc_solar_local_time
@@ -81,38 +81,37 @@ class TestLonSLT():
     def test_calc_solar_local_time(self, name):
         """Test calc_solar_local_time with longitudes from 0-360 deg"""
 
-        self.test_inst = pysat.Instrument(platform='pysat', name=name)
-        self.test_inst.load(date=self.test_time)
+        self.py_inst = pysat.Instrument(platform='pysat', name=name)
+        self.py_inst.load(date=self.inst_time)
 
-        coords.calc_solar_local_time(self.test_inst, lon_name="longitude",
+        coords.calc_solar_local_time(self.py_inst, lon_name="longitude",
                                      slt_name='slt')
 
         # This works because test instrument longitude ranges from 0-360 deg
-        assert (abs(self.test_inst['slt']
-                    - self.test_inst['longitude'] / 15.0)).max() < 1.0e-6
+        assert (abs(self.py_inst['slt']
+                    - self.py_inst['longitude'] / 15.0)).max() < 1.0e-6
 
     def test_calc_solar_local_time_w_neg_longitude(self):
         """Test calc_solar_local_time with longitudes from -180 to 180 deg"""
 
-        self.test_inst = pysat.Instrument(platform='pysat', name="testing")
-        self.test_inst.load(date=self.test_time)
+        self.py_inst = pysat.Instrument(platform='pysat', name="testing")
+        self.py_inst.load(date=self.inst_time)
 
-        coords.calc_solar_local_time(self.test_inst, lon_name="longitude",
+        coords.calc_solar_local_time(self.py_inst, lon_name="longitude",
                                      slt_name='slt')
-        coords.update_longitude(self.test_inst, lon_name="longitude")
-        coords.calc_solar_local_time(self.test_inst, lon_name="longitude",
+        coords.update_longitude(self.py_inst, lon_name="longitude")
+        coords.calc_solar_local_time(self.py_inst, lon_name="longitude",
                                      slt_name='slt2')
 
-        assert (abs(self.test_inst['slt']
-                    - self.test_inst['slt2'])).max() < 1.0e-6
+        assert (abs(self.py_inst['slt'] - self.py_inst['slt2'])).max() < 1.0e-6
 
     def test_bad_lon_name_calc_solar_local_time(self):
         """Test calc_solar_local_time with a bad longitude name"""
 
-        self.test_inst = pysat.Instrument(platform='pysat', name="testing")
-        self.test_inst.load(date=self.test_time)
+        self.py_inst = pysat.Instrument(platform='pysat', name="testing")
+        self.py_inst.load(date=self.inst_time)
 
         with pytest.raises(ValueError):
-            coords.calc_solar_local_time(self.test_inst,
+            coords.calc_solar_local_time(self.py_inst,
                                          lon_name="not longitude",
                                          slt_name='slt')
