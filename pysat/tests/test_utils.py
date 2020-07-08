1f8b9a189ded204c3918904e33dc339497d62a34
Jeff Klenzing <jklenzing@gmail.com>
2020-07-07 20:26:05 -0400

    STY: remove try/except around set_data_dir

--- pysat/tests/test_utils.py
+++ pysat/tests/test_utils.py
@@ -250,10 +250,7 @@ class TestBasicNetCDF4():
     def teardown(self):
         """Runs after every method to clean up previous testing."""
         remove_files(self.testInst)
-        try:
-            pysat.utils.set_data_dir(self.data_path, store=False)
-        except:
-            pass
+        pysat.utils.set_data_dir(self.data_path, store=False)
         del self.testInst
 
     @raises(ValueError)
@@ -496,10 +493,7 @@ class TestBasicNetCDF4xarray():
     def teardown(self):
         """Runs after every method to clean up previous testing."""
         remove_files(self.testInst)
-        try:
-            pysat.utils.set_data_dir(self.data_path, store=False)
-        except:
-            pass
+        pysat.utils.set_data_dir(self.data_path, store=False)
         del self.testInst
 
     def test_basic_write_and_read_netcdf4_default_format(self):
