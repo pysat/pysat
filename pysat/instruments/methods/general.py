2312d339f294b24fcd96830d872a51be2c0daeea
Jeff Klenzing <jklenzing@gmail.com>
2020-07-09 10:50:49 -0400

    BUG: fix bad merge

--- pysat/instruments/methods/general.py
+++ pysat/instruments/methods/general.py
@@ -4,10 +4,12 @@
 
 from __future__ import absolute_import, division, print_function
 
+import datetime as dt
 import logging
 import pandas as pds
 
 import pysat
+
 
 logger = logging.getLogger(__name__)
 
@@ -21,27 +23,27 @@ def list_files(tag=None, sat_id=None, data_path=None, format_str=None,
 
     Parameters
     -----------
-    tag : (string or NoneType)
+    tag : string or NoneType
         Denotes type of file to load.  Accepted types are <tag strings>.
         (default=None)
-    sat_id : (string or NoneType)
+    sat_id : string or NoneType
         Specifies the satellite ID for a constellation.  Not used.
         (default=None)
-    data_path : (string or NoneType)
+    data_path : string or NoneType
         Path to data directory.  If None is specified, the value previously
         set in Instrument.files.data_path is used.  (default=None)
-    format_str : (string or NoneType)
+    format_str : string or NoneType
         User specified file format.  If None is specified, the default
         formats associated with the supplied tags are used. (default=None)
-    supported_tags : (dict or NoneType)
+    supported_tags : dict or NoneType
         keys are sat_id, each containing a dict keyed by tag
         where the values file format template strings. (default=None)
-    fake_daily_files_from_monthly : (bool)
+    fake_daily_files_from_monthly : bool
         Some CDAWeb instrument data files are stored by month, interfering
         with pysat's functionality of loading by day. This flag, when true,
         appends daily dates to monthly files internally. These dates are
         used by load routine in this module to provide data by day.
-    two_digit_year_break : (int)
+    two_digit_year_break : int
         If filenames only store two digits for the year, then
         '1900' will be added for years >= two_digit_year_break
         and '2000' will be added for years < two_digit_year_break.
@@ -55,12 +57,12 @@ def list_files(tag=None, sat_id=None, data_path=None, format_str=None,
     --------
     ::
 
-        fname = 'cnofs_vefi_bfield_1sec_{year:04d}{month:02d}{day:02d}_v{version:02d}.cdf'
+        fname = 'cnofs_vefi_bfield_1sec_{year:04d}{month:02d}{day:02d}_v05.cdf'
         supported_tags = {'dc_b': fname}
         list_files = functools.partial(nasa_cdaweb.list_files,
                                        supported_tags=supported_tags)
 
-        fname = 'cnofs_cindi_ivm_500ms_{year:4d}{month:02d}{day:02d}_v{version:02d}.cdf'
+        fname = 'cnofs_cindi_ivm_500ms_{year:4d}{month:02d}{day:02d}_v01.cdf'
         supported_tags = {'': fname}
         list_files = functools.partial(mm_gen.list_files,
                                        supported_tags=supported_tags)
@@ -89,3 +91,73 @@ def list_files(tag=None, sat_id=None, data_path=None, format_str=None,
         estr = ''.join(('A directory must be passed to the loading routine ',
                         'for <Instrument Code>'))
         raise ValueError(estr)
+
+
+def convert_timestamp_to_datetime(inst, sec_mult=1.0):
+    """Use datetime instead of timestamp for Epoch
+
+    Parameters
+    ----------
+    inst : pysat.Instrument
+        associated pysat.Instrument object
+    sec_mult : float
+        Multiplier needed to convert epoch time to seconds (default=1.0)
+
+    Returns
+    -------
+    None
+        Modifies Instrument object in place
+
+
+    """
+
+    inst.data['Epoch'] = \
+        pds.to_datetime([dt.datetime.utcfromtimestamp(x * sec_mult)
+                         for x in inst.data['Epoch']])
+    return
+
+
+def remove_leading_text(inst, target=None):
+    """Removes leading text on variable names
+
+    Parameters
+    ----------
+    inst : pysat.Instrument
+        associated pysat.Instrument object
+    target : str
+        Leading string to remove. If none supplied,
+        ICON project standards are used to identify and remove
+        leading text
+
+    Returns
+    -------
+    None
+        Modifies Instrument object in place
+
+
+    """
+
+    if target is not None:
+        prepend_str = target
+
+        if isinstance(inst.data, pds.DataFrame):
+            inst.data.rename(columns=lambda x: x.split(prepend_str)[-1],
+                             inplace=True)
+        else:
+            map = {}
+            for key in inst.data.variables.keys():
+                map[key] = key.split(prepend_str)[-1]
+            inst.data = inst.data.rename(name_dict=map)
+
+        inst.meta.data.rename(index=lambda x: x.split(prepend_str)[-1],
+                              inplace=True)
+        orig_keys = inst.meta.keys_nD()
+        for keynd in orig_keys:
+            if keynd.find(prepend_str) >= 0:
+                new_key = keynd.split(prepend_str)[-1]
+                new_meta = inst.meta.pop(keynd)
+                new_meta.data.rename(index=lambda x: x.split(prepend_str)[-1],
+                                     inplace=True)
+                inst.meta[new_key] = new_meta
+
+    return
