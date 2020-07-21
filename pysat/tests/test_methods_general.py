0593c3eb1baa827f7b5e44861306ca56f1e02b66
Jeff Klenzing <jklenzing@gmail.com>
2020-07-21 12:15:22 -0400

    TST: update general methods tests

--- pysat/tests/test_methods_general.py
+++ pysat/tests/test_methods_general.py
@@ -1,3 +1,5 @@
+from nose import raises
+
 import pysat
 from pysat.instruments.methods import general as gen
 
@@ -16,11 +18,11 @@ class TestGenMethods():
         del self.kwargs
 
 
-class TestICONIVMCustom():
+class TestRemoveLeadText():
     def setup(self):
         """Runs before every method to create a clean testing setup."""
         # Load a test instrument
-        self.testInst = pysat.Instrument('pysat', 'testing', tag='12',
+        self.testInst = pysat.Instrument('pysat', 'testing', sat_id='12',
                                          clean_level='clean')
         self.testInst.load(2009, 1)
         self.Npts = len(self.testInst['uts'])
@@ -29,10 +31,12 @@ class TestICONIVMCustom():
         """Runs after every method to clean up previous testing."""
         del self.testInst, self.Npts
 
-    def test_remove_names_wo_target(self):
-        self.testInst.tag = ''
-        self.testInst.sat_id = 'a'
+    @raises(ValueError)
+    def test_remove_prefix_w_bad_target(self):
+        self.testInst['ICON_L27_Blurp'] = self.testInst['dummy1']
+        gen.remove_leading_text(self.testInst, target=17.5)
 
+    def test_remove_names_wo_target(self):
         self.testInst['ICON_L27_Blurp'] = self.testInst['dummy1']
         gen.remove_leading_text(self.testInst)
         # check variables unchanged
@@ -40,23 +44,37 @@ class TestICONIVMCustom():
         # check other names untouched
         assert (len(self.testInst['dummy1']) == self.Npts)
 
-    def test_remove_names_target(self):
-        self.testInst.tag = ''
-        self.testInst.sat_id = 'a'
-
+    def test_remove_names_w_target(self):
         self.testInst['ICON_L27_Blurp'] = self.testInst['dummy1']
         gen.remove_leading_text(self.testInst, target='ICON_L27')
         # check prepended text removed
-        assert (len(self.testInst['_Blurp']) == self.Npts)
+        assert len(self.testInst['_Blurp']) == self.Npts
         # check other names untouched
-        assert (len(self.testInst['dummy1']) == self.Npts)
+        assert len(self.testInst['dummy1']) == self.Npts
+        # check prepended text removed from metadata
+        assert '_Blurp' in self.testInst.meta.keys()
+
+    def test_remove_names_w_target_list(self):
+        self.testInst['ICON_L27_Blurp'] = self.testInst['dummy1']
+        self.testInst['ICON_L23_Bloop'] = self.testInst['dummy1']
+        gen.remove_leading_text(self.testInst,
+                                target=['ICON_L27', 'ICON_L23_B'])
+        # check prepended text removed
+        assert len(self.testInst['_Blurp']) == self.Npts
+        assert len(self.testInst['loop']) == self.Npts
+        # check other names untouched
+        assert len(self.testInst['dummy1']) == self.Npts
+        # check prepended text removed from metadata
+        assert '_Blurp' in self.testInst.meta.keys()
+        assert 'loop' in self.testInst.meta.keys()
 
 
-class TestRemoveLeadTextXarray(TestICONIVMCustom):
+class TestRemoveLeadTextXarray(TestRemoveLeadText):
     def setup(self):
         """Runs before every method to create a clean testing setup."""
         # Load a test instrument
-        self.testInst = pysat.Instrument('pysat', 'testing_xarray', tag='12',
+        self.testInst = pysat.Instrument('pysat', 'testing2d_xarray',
+                                         sat_id='12',
                                          clean_level='clean')
         self.testInst.load(2009, 1)
         self.Npts = len(self.testInst['uts'])
@@ -64,3 +82,22 @@ class TestRemoveLeadTextXarray(TestICONIVMCustom):
     def teardown(self):
         """Runs after every method to clean up previous testing."""
         del self.testInst, self.Npts
+
+    def test_remove_2D_names_w_target(self):
+        gen.remove_leading_text(self.testInst, target='variable')
+        # check prepended text removed from variables
+        assert '_profiles' in self.testInst.data.variables
+        assert self.testInst.data['_profiles'].shape[0] == self.Npts
+        # check prepended text removed from metadata
+        assert '_profiles' in self.testInst.meta.keys()
+
+    def test_remove_2D_names_w_target_list(self):
+        gen.remove_leading_text(self.testInst,
+                                target=['variable', 'im'])
+        # check prepended text removed from variables
+        assert '_profiles' in self.testInst.data.variables
+        assert self.testInst.data['_profiles'].shape[0] == self.Npts
+        assert 'ages' in self.testInst.data.variables
+        # check prepended text removed from metadata
+        assert '_profiles' in self.testInst.meta.keys()
+        assert 'ages' in self.testInst.meta.keys()
