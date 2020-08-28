6855c57a6399b581385728aca536df8f7d295554
Russell Stoneback <rstoneba@utdallas.edu>
2020-08-28 11:05:57 -0500

    ENH: First draft for unit testing

--- pysat/tests/test_registry.py
+++ pysat/tests/test_registry.py
@@ -41,53 +41,165 @@ def create_fake_module(full_module_name, platform, name):
     sys.modules['.'.join([package_name, module_name])] = instrument
 
 
+def ensure_updated_stored_modules(modules):
+    """Ensure stored pysat.user_modules updated
+    to include modules
+
+    Parameters
+    ----------
+    modules : list
+        List of tuples (module_string, platform, name)
+        that will be checked against stored
+        pysat.user_modules
+
+    """
+    # make sure filesystem was updated
+    saved_modules = registry.load_saved_modules()
+    for module_name, platform, name in modules:
+        assert platform in saved_modules
+        assert name in saved_modules[platform]
+        assert module_name in saved_modules[platform][name]
+
+
+def ensure_live_registry_updated(modules):
+    """Ensure current pysat.user_modules updated
+    to include modules
+
+    Parameters
+    ----------
+    modules : list
+        List of tuples (module_string, platform, name)
+        that will be checked against stored
+        pysat.user_modules
+
+    """
+
+    for module_name, platform, name in modules:
+        # check that global registry was updated
+        assert platform in user_modules
+        assert name in user_modules[platform]
+        assert module_name in user_modules[platform][name]
+
+
+def ensure_not_in_stored_modules(modules):
+    """Ensure modules not in stored pysat.user_modules
+
+    Parameters
+    ----------
+    modules : list
+        List of tuples (module_string, platform, name)
+        that will be checked against stored
+        pysat.user_modules
+
+    """
+    # ensure input modules not in stored modules
+    saved_modules = registry.load_saved_modules()
+    for module_name, platform, name in modules:
+        if platform in saved_modules:
+            assert module_name not in saved_modules[platform]
+        else:
+            # platform not present, so not registered
+            assert True
+        with pytest.raises(KeyError):
+            Instrument(platform, name)
+
+
+def test_registration():
+    modules = [
+        ('package1.module1', 'platname1', 'name1'),
+        ('package2.module2', 'platname2', 'name2')]
+
+    module_names = [mod[0] for mod in modules]
+    platforms = [mod[1] for mod in modules]
+    platform_names = [mod[2] for mod in modules]
+
+    # do pre-cleanup
+    try:
+        registry.remove(platforms, platform_names)
+    except ValueError:
+        pass
+
+    # make sure instruments are not yet registered
+    ensure_not_in_stored_modules(modules)
+
+    # create modules
+    for module_name, platform, name in modules:
+        # create modules
+        create_fake_module(module_name, platform, name)
+        # import fake modules
+        test_mod = importlib.import_module(module_name)
+
+        # load module by keyword
+        inst = Instrument(inst_module=test_mod)
+        # ensure we have the correct one
+        assert inst.platform == platform
+        assert inst.name == name
+
+        # register package
+        registry.register(module_name)
+        # load by platform and name
+        inst2 = Instrument(platform, name)
+        # ensure we have the correct one
+        assert inst2.platform == platform
+        assert inst2.name == name
+
+        # check that global registry was updated
+        ensure_live_registry_updated([(module_name, platform, name)])
+
+    # verify update
+    ensure_updated_stored_modules(modules)
+    # clean up
+    registry.remove(platforms, platform_names)
+    # ensure things are clean
+    ensure_not_in_stored_modules(modules)
+
+
 def test_multi_registration():
     modules = [
         ('package1.module1', 'platname1', 'name1'),
         ('package2.module2', 'platname2', 'name2')]
 
     module_names = [mod[0] for mod in modules]
+    platforms = [mod[1] for mod in modules]
+    platform_names = [mod[2] for mod in modules]
 
-    registry.remove(*module_names)
-
-    # make sure modules are not yet created
-    for module_name in module_names:
-        with pytest.raises(ImportError):
-            importlib.import_module(module_name)
+    # do pre-cleanup
+    try:
+        registry.remove(platforms, platform_names)
+    except ValueError:
+        pass
 
     # make sure instruments are not yet registered
-    saved_modules = registry.load_saved_modules()
-    for module_name, platform, name in modules:
-        assert module_name not in saved_modules
-        with pytest.raises(ImportError):
-            Instrument(platform, name)
+    ensure_not_in_stored_modules(modules)
 
     # create modules
     for module_name, platform, name in modules:
-
+        # create modules
         create_fake_module(module_name, platform, name)
-
+        # import fake modules
         test_mod = importlib.import_module(module_name)
-
         # load module by keyword
-        Instrument(inst_module=test_mod)
+        inst = Instrument(inst_module=test_mod)
+        # ensure we have the correct one
+        assert inst.platform == platform
+        assert inst.name == name
 
+    # register all modules at once
+    registry.register(module_names)
+    # verify registration
+    ensure_live_registry_updated(modules)
+    # verify stored update
+    ensure_updated_stored_modules(modules)
+
+    # verify instantiation
+    for module_name, platform, name in modules:
         # load by platform and name
-        registry.register(module_name)
-        Instrument(platform, name)
-
-        # check that global registry was updated
-        assert module_name in user_modules
-
-    # make sure user_modules.txt was updated
-    saved_modules = registry.load_saved_modules()
-    for module_name in module_names:
-        assert module_name in saved_modules
+        inst2 = Instrument(platform, name)
+        # ensure we have the correct one
+        assert inst2.platform == platform
+        assert inst2.name == name
 
     # clean up
-    registry.remove(*module_names)
-
-    saved_modules = registry.load_saved_modules()
-    for module_name in module_names:
-        assert module_name not in user_modules
-        assert module_name not in saved_modules
+    registry.remove(platforms, platform_names)
+    # ensure things are clean
+    ensure_not_in_stored_modules(modules)
