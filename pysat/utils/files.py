669d96b5737119ce5b7e1466e64c89164569a13a
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2020-10-13 12:42:54 -0400

    STY: descriptive variables
    Updated variable names to be descriptive.

--- pysat/utils/files.py
+++ pysat/utils/files.py
@@ -234,15 +234,16 @@ def parse_delimited_filenames(files, format_str, delimiter):
     # so apply delimiter breakdown to the string blocks as a guide
     pblock = []
     parsed_block = [snip.split(delimiter) for snip in snips]
-    for _ in parsed_block:
-        if _ != ['', '']:
-            if _[0] == '':
-                _ = _[1:]
-            if _[-1] == '':
-                _ = _[:-1]
-            pblock.extend(_)
+    for block in parsed_block:
+        if block != ['', '']:
+            if block[0] == '':
+                block = block[1:]
+            if block[-1] == '':
+                block = block[:-1]
+            pblock.extend(block)
         pblock.append('')
     parsed_block = pblock[:-1]
+
     # need to parse out dates for datetime index
     for temp in files:
         split_name = temp.split(delimiter)
@@ -264,8 +265,10 @@ def parse_delimited_filenames(files, format_str, delimiter):
             stored[key] = np.array(stored[key])
         if len(stored[key]) == 0:
             stored[key] = None
+
     # include files in output
     stored['files'] = files
+
     # include format string as convenience for later functions
     stored['format_str'] = format_str
 
