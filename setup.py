f00f646316ca4fdf0d66294cdf04a72e70cc5c26
Jeff Klenzing <jklenzing@gmail.com>
2020-11-18 12:29:59 -0500

    DOC: move setup metadata to cfg

--- setup.py
+++ setup.py
@@ -1,88 +1,12 @@
-"""A setuptools based setup module.
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2020, Authors
+# Full license can be found in License.md and AUTHORS.md
+# Full license can be found in License.md
+# -----------------------------------------------------------------------------
 
-# Always prefer setuptools over distutils
 from setuptools import setup
-# To use a consistent encoding
-import codecs
-import os
 
-here = os.path.abspath(os.path.dirname(__file__))
-with codecs.open(os.path.join(here, 'description.txt'), encoding='utf-8') as f:
-    long_description = f.read()
-version_filename = os.path.join('pysat', 'version.txt')
-with codecs.open(os.path.join(here, version_filename)) as version_file:
-    version = version_file.read().strip()
+# package metadata stored in setup.cfg
 
-# packages to be installed
-# starting with packages common across all setups
-install_requires = ['numpy>=1.12', 'scipy', 'pandas>=0.23', 'xarray',
-                    'netCDF4', 'portalocker', 'dask', 'toolz']
-
-# flag, True if on readthedocs
-on_rtd = os.environ.get('READTHEDOCS') == 'True'
-
-setup(
-    name='pysat',
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version=version,
-
-    description='Supports science data analysis across measurement platforms',
-    long_description=long_description,
-    # The project's main homepage.
-    url='http://github.com/pysat/pysat',
-
-    # Author details
-    author='Russell Stoneback',
-    author_email='rstoneba@utdallas.edu',
-
-
-    package_data={'pysat': ['pysat/version*.txt']},
-    include_package_data=True,
-
-    # Choose your license
-    license='BSD',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 5 - Production/Stable',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Astronomy',
-        'Topic :: Scientific/Engineering :: Physics',
-        'Topic :: Scientific/Engineering :: Atmospheric Science',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: BSD License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
-
-    # What does your project relate to?
-    # keywords='sample setuptools development',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=['pysat', 'pysat.instruments', 'pysat.utils'],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=install_requires,
-)
+setup()
