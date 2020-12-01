7d7c277d2c9ad3e178d2458625c3f2449f86e5b3
Jeff Klenzing <jklenzing@gmail.com>
2020-12-01 11:26:55 -0500

    DOC: updates from review

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Copyright (C) 2020, Authors
-# Full license can be found in License.md and AUTHORS.md
 # Full license can be found in License.md
 # -----------------------------------------------------------------------------
 
