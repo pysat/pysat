d9732edc39f08c574bd1d7fc90bc719cf2012aea
Jeff Klenzing <jklenzing@gmail.com>
2020-12-02 20:49:22 -0500

    BUG: index standards

--- pysat/instruments/pysat_testing_xarray.py
+++ pysat/instruments/pysat_testing_xarray.py
@@ -123,16 +123,16 @@ def load(fnames, tag=None, inst_id=None, sim_multi_file_right=False,
     else:
         root_date = dt.datetime(2009, 1, 1, tzinfo=dt.timezone.utc)
 
+    index = index.tolist()
     if malformed_index:
-        index = index.tolist()
         # nonmonotonic
         index[0:3], index[3:6] = index[3:6], index[0:3]
         # non unique
         index[6:9] = [index[6]] * 3
 
-    data = xarray.Dataset({'uts': ((epoch_name), index.tolist())},
-                          coords={epoch_name: index.tolist()})
-    
+    data = xarray.Dataset({'uts': ((epoch_name), index)},
+                          coords={epoch_name: index})
+
     # need to create simple orbits here. Have start of first orbit
     # at 2009,1, 0 UT. 14.84 orbits per day
     time_delta = dates[0] - root_date
