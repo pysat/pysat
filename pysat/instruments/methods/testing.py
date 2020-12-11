941c3764e1d0f57f9d48fe4c48fe864c8b84b649
Jeff Klenzing <jklenzing@gmail.com>
2020-12-11 12:38:15 -0500

    DOC: comments

--- pysat/instruments/methods/testing.py
+++ pysat/instruments/methods/testing.py
@@ -141,8 +141,10 @@ def download(date_array, tag, inst_id, data_path=None, user=None,
 
     if tag == 'no_download':
         warnings.warn('This simulates an instrument without download support')
+    # Check that user name and password are passed through the unit tests
     if tag == 'user_password':
         if (not user) and (not password):
+            # Note that this line will be uncovered if test succeeds
             raise ValueError(' '.join(('Instrument is not passing user and',
                                        'password to test instruments')))
 
