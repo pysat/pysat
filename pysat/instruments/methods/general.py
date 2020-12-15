2814ce3935dcae9d50e6c23e15cdf9b295e9c565
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2020-12-15 09:46:04 -0500

    STY: replaced fake_daily_files_from monthly
    Replaced fake_daily_files_from_monthly with a more
    adaptive keyword input that allows any regular file
    cadance greater or equal to one day.

--- pysat/instruments/methods/general.py
+++ pysat/instruments/methods/general.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 
 
 def list_files(tag=None, inst_id=None, data_path=None, format_str=None,
-               supported_tags=None, fake_daily_files_from_monthly=False,
+               supported_tags=None, file_cadance=dt.timedelta(days=1),
                two_digit_year_break=None, delimiter=None):
     """Return a Pandas Series of every file for chosen Instrument data.
 
@@ -37,11 +37,11 @@ def list_files(tag=None, inst_id=None, data_path=None, format_str=None,
     supported_tags : dict or NoneType
         keys are inst_id, each containing a dict keyed by tag
         where the values file format template strings. (default=None)
-    fake_daily_files_from_monthly : bool
-        Some instrument data files are stored by month, interfering with daily
-        file loading assumed by pysat. If True, this flag appends daily dates
-        to monthly files internally. These dates are used by load routine in
-        this module to provide data by day. (default=False)
+    file_cadance : dt.timedelta or pds.DateOffset
+        pysat assumes a daily file cadance, but some instrument data file
+        contain longer periods of time.  This parameter allows the specification
+        of regular file cadances greater than or equal to a day (e.g., weekly,
+        monthly, or yearly). (default=dt.timedelta(days=1))
     two_digit_year_break : int or NoneType
         If filenames only store two digits for the year, then '1900' will be
         added for years >= two_digit_year_break and '2000' will be added for
@@ -90,11 +90,11 @@ def list_files(tag=None, inst_id=None, data_path=None, format_str=None,
                               two_digit_year_break=two_digit_year_break,
                               delimiter=delimiter)
 
-    # If the data is monthly, pad the series
-    # TODO: take file frequency as an input to allow e.g., weekly files
-    if (not out.empty) and fake_daily_files_from_monthly:
+    # If the data is not daily, pad the series.  Both pds.DateOffset and
+    # dt.timedelta contain the 'days' attribute, so evaluate using that
+    if (not out.empty) and file_cadance.days > 1:
         emonth = out.index[-1]
-        out.loc[out.index[-1] + pds.DateOffset(months=1)
+        out.loc[out.index[-1] + file_cadance
                 - dt.timedelta(days=1)] = out.iloc[-1]
         new_out = out.asfreq('D')
 
