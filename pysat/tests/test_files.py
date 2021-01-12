12e5773c89936e4ad5c5c936d4caa851e28ce6d2
Russell Stoneback <rstoneba@utdallas.edu>
2021-01-12 15:27:41 -0600

    TST: Updated TravisCI test test_files

--- pysat/tests/test_files.py
+++ pysat/tests/test_files.py
@@ -1012,30 +1012,33 @@ class TestCIonly():
         if not self.ci_env:
             pytest.skip("Skipping local tests to avoid breaking user setup")
 
+        # Move settings directory to simulate first load after install
+        self.root = os.path.join(os.getenv('HOME'), '.pysat')
+        self.new_root = os.path.join(os.getenv('HOME'), '.saved_pysat')
+        shutil.move(self.root, self.new_root)
+
     def teardown(self):
         """Runs after every method to clean up previous testing."""
         del self.ci_env
 
+        # Move settings back
+        shutil.rmtree(self.root)
+        shutil.move(self.new_root, self.root)
+
     def test_initial_pysat_load(self, capsys):
         """Ensure data_dirs check in Files works"""
 
-        # Move settings directory to simulate first load after install
-        root = os.path.join(os.getenv('HOME'), '.pysat')
-        new_root = os.path.join(os.getenv('HOME'), '.saved_pysat')
-        shutil.move(root, new_root)
+        # Make sure there is no suitable data directory set
+        pysat.params.data['data_dirs'] = []
 
-        reload(pysat)
-
-        captured = capsys.readouterr()
         # Ensure pysat is running in 'first-time' mode
+        reload(pysat)
+        captured = capsys.readouterr()
         assert captured.out.find("Hi there!") >= 0
 
-        # try to instantiate Instrument
+        # Try to instantiate Instrument
         with pytest.raises(RuntimeError) as err:
             pysat.Instrument('pysat', 'testing')
 
+        # Confirm we have the correct error
         assert str(err).find("pysat's `data_dirs` hasn't been set.") >= 0
-
-        # Move settings back
-        shutil.rmtree(root)
-        shutil.move(new_root, root)
