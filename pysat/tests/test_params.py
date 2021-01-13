cf5eef664ef105c2f11040f38de03cc53be063ac
Russell Stoneback <rstoneba@utdallas.edu>
2021-01-13 15:50:23 -0600

    TST: Created common TravisCI test setup class

--- pysat/tests/test_params.py
+++ pysat/tests/test_params.py
@@ -9,67 +9,68 @@ tests the pysat utils area
 
 import copy
 from importlib import reload
-import numpy as np
 import os
 import pytest
 import shutil
-import warnings
 
-import pysat
-from pysat._params import Parameters
+import pysat  # required for reimporting pysat
+from pysat import params
+from pysat._params import Parameters  # required for eval statements
+from pysat.tests.travisci_test_class import TravisCICleanSetup
+
 
 class TestBasics():
     def setup(self):
         """Runs before every method to create a clean testing setup."""
         # store current pysat directory
-        self.stored_params = copy.deepcopy(pysat.params)
+        self.stored_params = copy.deepcopy(params)
         # set up default values
-        pysat.params.restore_defaults()
+        params.restore_defaults()
 
     def teardown(self):
         """Runs after every method to clean up previous testing."""
-        pysat.params = self.stored_params
-        pysat.params.store()
+        params = self.stored_params
+        params.store()
 
     #######################
     # test pysat data dir options
     def test_set_data_dirs_param_single(self):
-        """Update pysat directory via pysat.params, single string input"""
-        pysat.params['data_dirs'] = '.'
-        assert pysat.params['data_dirs'] == ['.']
+        """Update pysat directory via params, single string input"""
+        params['data_dirs'] = '.'
+        assert params['data_dirs'] == ['.']
 
         # Check if next load of pysat remembers the change
         reload(pysat)
-        assert pysat.params['data_dirs'] == ['.']
+        assert params['data_dirs'] == ['.']
 
     def test_set_data_dirs_param_with_list(self):
         """Update pysat directories via pysat.params, list of strings"""
-        pysat.params['data_dirs'] = ['.', './']
-        assert pysat.params['data_dirs'] == ['.', './']
+        params['data_dirs'] = ['.', './']
+        assert params['data_dirs'] == ['.', './']
 
         # Check if next load of pysat remembers the change
         reload(pysat)
-        assert pysat.params['data_dirs'] == ['.', './']
+        assert params['data_dirs'] == ['.', './']
 
     def test_set_data_dir_wrong_path(self):
         """Update data_dir with an invalid path form"""
         with pytest.raises(ValueError):
-            pysat.params['data_dirs'] = 'not_a_directory'
+            params['data_dirs'] = 'not_a_directory'
 
     def test_set_data_dir_bad_directory(self):
         """Ensure you can't set data directory to bad path"""
         with pytest.raises(ValueError) as excinfo:
-            pysat.params['data_dirs'] = '/fake/directory/path'
+            params['data_dirs'] = '/fake/directory/path'
         assert str(excinfo.value).find("don't lead to a valid") >= 0
 
     def test_repr(self):
         """Test __repr__ method"""
-        out = pysat.params.__repr__()
+        out = params.__repr__()
         assert out.find('Parameters(path=') >= 0
 
     def test_str(self):
         """Ensure str method works"""
-        out = str(pysat.params)
+        out = str(params)
         # Confirm start of str
         assert out.find('pysat Parameters object') >= 0
 
@@ -87,91 +88,80 @@ class TestBasics():
         """Test restore_defaults works as intended"""
 
         # Get default value, as per setup
-        default_val = pysat.params['update_files']
+        default_val = params['update_files']
 
         # Change value to non-default
-        pysat.params['update_files'] = not default_val
+        params['update_files'] = not default_val
 
         # Restore defaults
-        pysat.params.restore_defaults()
+        params.restore_defaults()
 
         # Ensure new value is the default
-        assert pysat.params['update_files'] == default_val
+        assert params['update_files'] == default_val
 
         # make sure that non-default values left as is
-        assert pysat.params['data_dirs'] != []
+        assert params['data_dirs'] != []
 
     def test_update_standard_value(self):
         """Modify a pre-existing standard parameter value and ensure stored"""
 
         # Get default value, as per setup
-        default_val = pysat.params['update_files']
+        default_val = params['update_files']
 
         # Change value to non-default
-        pysat.params['update_files'] = not pysat.params['update_files']
+        params['update_files'] = not params['update_files']
 
         # Ensure it is in memory
-        assert pysat.params['update_files'] is not default_val
+        assert params['update_files'] is not default_val
 
         # Get a new parameters instance and verify information is retained
-        new_params = eval(pysat.params.__repr__())
-        assert new_params['update_files'] == pysat.params['update_files']
+        new_params = eval(params.__repr__())
+        assert new_params['update_files'] == params['update_files']
 
     def test_no_update_user_modules(self):
         """Ensure user_modules not modifiable via params"""
 
         # Attempt to change value
         with pytest.raises(ValueError) as err:
-            pysat.params['user_modules'] = {}
+            params['user_modules'] = {}
         assert str(err).find('The pysat.utils.registry ') >= 0
 
     def test_add_user_parameter(self):
         """Add custom parameter and ensure present"""
 
-        pysat.params['hi_there'] = 'hello there!'
-        assert pysat.params['hi_there'] == 'hello there!'
+        params['hi_there'] = 'hello there!'
+        assert params['hi_there'] == 'hello there!'
 
         # Get a new parameters instance and verify information is retained
-        new_params = eval(pysat.params.__repr__())
-        assert new_params['hi_there'] == pysat.params['hi_there']
+        new_params = eval(params.__repr__())
+        assert new_params['hi_there'] == params['hi_there']
 
     def test_clear_and_restart(self):
         """Verify clear_and_restart method"""
 
-        pysat.params.clear_and_restart()
+        params.clear_and_restart()
 
         # check default value
-        assert pysat.params['user_modules'] == {}
+        assert params['user_modules'] == {}
 
         # check value without working default
-        assert pysat.params['data_dirs'] == []
+        assert params['data_dirs'] == []
 
         return
 
 
-class TestCIonly():
+class TestCIonly(TravisCICleanSetup):
     """Tests where we mess with local settings.
     These only run in CI environments such as Travis and Appveyor to avoid
     breaking an end user's setup
     """
 
-    def setup(self):
-        """Runs before every method to create a clean testing setup."""
-        self.ci_env = (os.environ.get('TRAVIS') == 'true')
-        if not self.ci_env:
-            pytest.skip("Skipping local tests to avoid breaking user setup")
-
-    def teardown(self):
-        """Runs after every method to clean up previous testing."""
-        del self.ci_env
+    # Set setup/teardown to the class defaults
+    setup = TravisCICleanSetup.setup
+    teardown = TravisCICleanSetup.teardown
 
     def test_initial_pysat_parameters_load(self, capsys):
         """Ensure initial parameters load routines work"""
-
-        # Move settings directory to simulate first load after install
-        root = os.path.join(os.getenv('HOME'), '.pysat')
-        new_root = os.path.join(os.getenv('HOME'), '.saved_pysat')
-        shutil.move(root, new_root)
 
         reload(pysat)
 
@@ -180,29 +170,25 @@ class TestCIonly():
         assert captured.out.find("Hi there!") >= 0
 
         # Remove pysat settings file
-        shutil.move(os.path.join(root, 'pysat_settings.json'),
-                    os.path.join(root, 'pysat_settings_moved.json'))
+        shutil.move(os.path.join(self.root, 'pysat_settings.json'),
+                    os.path.join(self.root, 'pysat_settings_moved.json'))
 
         # Ensure we can't create a parameters file without valid .json
         with pytest.raises(RuntimeError) as err:
-            pysat._params.Parameters()
+            Parameters()
         assert str(err).find('pysat is unable to locate a user settings') >= 0
 
         # Move pysat settings file to cwd and try again
-        shutil.move(os.path.join(root, 'pysat_settings_moved.json'),
+        shutil.move(os.path.join(self.root, 'pysat_settings_moved.json'),
                     os.path.join('./', 'pysat_settings.json'))
 
-        pysat._params.Parameters()
+        Parameters()
 
         # Move pysat settings file back to original
         shutil.move(os.path.join('./', 'pysat_settings.json'),
-                    os.path.join(root, 'pysat_settings.json'))
+                    os.path.join(self.root, 'pysat_settings.json'))
 
         # Make sure settings file created
-        assert os.path.isfile(os.path.join(root, 'pysat_settings.json'))
-        assert os.path.isdir(os.path.join(root, 'instruments'))
-        assert os.path.isdir(os.path.join(root, 'instruments', 'archive'))
-
-        # Move settings back
-        shutil.rmtree(root)
-        shutil.move(new_root, root)
+        assert os.path.isfile(os.path.join(self.root, 'pysat_settings.json'))
+        assert os.path.isdir(os.path.join(self.root, 'instruments'))
+        assert os.path.isdir(os.path.join(self.root, 'instruments', 'archive'))
