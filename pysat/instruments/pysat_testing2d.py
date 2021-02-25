840d4e6237568d39a3cca8b3b18034d215e05714
Russell Stoneback <rstoneba@utdallas.edu>
2021-02-25 10:37:59 -0600

    STY: pysat.logger

--- pysat/instruments/pysat_testing2d.py
+++ pysat/instruments/pysat_testing2d.py
@@ -14,7 +14,7 @@ import pandas as pds
 import pysat
 from pysat.instruments.methods import testing as mm_test
 
-logger = logging.getLogger(__name__)
+logger = pysat.logger
 
 platform = 'pysat'
 name = 'testing2d'
--- pysat/instruments/pysat_testmodel.py
+++ pysat/instruments/pysat_testmodel.py
@@ -14,7 +14,7 @@ import xarray as xr
 import pysat
 from pysat.instruments.methods import testing as mm_test
 
-logger = logging.getLogger(__name__)
+logger = pysat.logger
 
 platform = 'pysat'
 name = 'testmodel'
