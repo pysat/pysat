0f3f458962ea4d5a8a18c7c332ad3be8afb1a307
Jeff Klenzing <jklenzing@gmail.com>
2021-03-19 17:45:42 -0400

    DEP: constellations

--- pysat/constellations/de2.py
+++ pysat/constellations/de2.py
@@ -1,8 +1,23 @@
-import pysat
 """
-Creates a constellation from NASA ICON instrumentation
+Creates a constellation from NASA DE2 instrumentation
+
+.. deprecated:: 2.3.0
+  This routine has been deprecated in pysat 3.0.0, and will be accessible
+  in `pysatNASA.constellations.de2`
+
 """
 
+import warnings
+
+import pysat
+
+
+warnings.warn(" ".join(["This constellation",
+                        "has been removed from the pysat-managed",
+                        "Instruments in pysat 3.0.0, and now resides in",
+                        "pysatNASA:",
+                        "https://github.com/pysat/pysatNASA"]),
+              DeprecationWarning, stacklevel=2)
 
 lang = pysat.Instrument(platform='de2', name='lang')
 nacs = pysat.Instrument(platform='de2', name='nacs')
--- pysat/constellations/icon.py
+++ pysat/constellations/icon.py
@@ -1,8 +1,23 @@
-import pysat
 """
 Creates a constellation from NASA ICON instrumentation
+
+.. deprecated:: 2.3.0
+  This routine has been deprecated in pysat 3.0.0, and will be accessible
+  in `pysatNASA.constellations.icon`
+
 """
 
+import warnings
+
+import pysat
+
+
+warnings.warn(" ".join(["This constellation",
+                        "has been removed from the pysat-managed",
+                        "Instruments in pysat 3.0.0, and now resides in",
+                        "pysatNASA:",
+                        "https://github.com/pysat/pysatNASA"]),
+              DeprecationWarning, stacklevel=2)
 
 ivm = pysat.Instrument('icon', 'ivm', sat_id='a', tag='level_2',
                        clean_level='clean', update_files=True)
