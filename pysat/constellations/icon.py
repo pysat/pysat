2463ebf254c9c02f69dacd865132d9a3a85b40cf
Jeff Klenzing <jklenzing@gmail.com>
2021-03-22 13:36:55 -0400

    STY: simple constellation

--- pysat/constellations/icon.py
+++ pysat/constellations/icon.py
@@ -9,16 +9,9 @@ Creates a constellation from NASA ICON instrumentation
 
 import pysat
 
-ivm = pysat.Instrument('icon', 'ivm', sat_id='a', tag='level_2',
+ivm = pysat.Instrument('icon', 'ivm', sat_id='a', tag='',
                        clean_level='clean', update_files=True)
-euv = pysat.Instrument('icon', 'euv', sat_id='', tag='level_2',
+euv = pysat.Instrument('icon', 'euv', sat_id='', tag='',
                        clean_level='clean', update_files=True)
-fuv = pysat.Instrument('icon', 'fuv', sat_id='', tag='level_2',
-                       clean_level='clean', update_files=True)
-mighti_green = pysat.Instrument('icon', 'mighti', sat_id='green',
-                                tag='level_2', clean_level='clean',
-                                update_files=True)
-mighti_red = pysat.Instrument('icon', 'mighti', sat_id='red', tag='level_2',
-                              clean_level='clean', update_files=True)
 
-instruments = [ivm, euv, fuv, mighti_green, mighti_red]
+instruments = [ivm, euv]
