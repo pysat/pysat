0d9a13bd562db925cecee32a11241bee0812699c
Jeff Klenzing <jklenzing@gmail.com>
2021-05-05 15:44:51 -0400

    STY: use of noqa

--- pysat/instruments/methods/__init__.py
+++ pysat/instruments/methods/__init__.py
@@ -5,5 +5,5 @@ specific functions for instruments and classes of instruments.
 Each set of methods is contained within a subpackage of this set.
 """
 
-from pysat.instruments.methods import general  # noqa F401
-from pysat.instruments.methods import testing  # noqa F401
+from pysat.instruments.methods import general
+from pysat.instruments.methods import testing
--- pysat/utils/__init__.py
+++ pysat/utils/__init__.py
@@ -9,8 +9,8 @@ of formats, loading of files, and user-supplied info
 for the pysat data directory structure.
 """
 
-from pysat.utils import coords, files, time, registry, testing  # noqa F401
-from pysat.utils._core import scale_units, load_netcdf4  # noqa F401
-from pysat.utils._core import NetworkLock, generate_instrument_list  # noqa F401
-from pysat.utils._core import available_instruments  # noqa F401
-from pysat.utils._core import display_available_instruments  # noqa F401
+from pysat.utils import coords, files, time, registry, testing
+from pysat.utils._core import scale_units, load_netcdf4
+from pysat.utils._core import NetworkLock, generate_instrument_list
+from pysat.utils._core import available_instruments
+from pysat.utils._core import display_available_instruments
