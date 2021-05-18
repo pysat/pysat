dc36408d0d03f5f453ee100c6d20f0703eb73646
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2021-05-18 08:51:31 -0400

    TST: added Constellation unit tests
    Added more Constellation unit tests, guided by converalls.

--- pysat/tests/test_constellation.py
+++ pysat/tests/test_constellation.py
@@ -21,11 +21,12 @@ class TestConstellationInit:
         self.in_kwargs = {"instruments": self.instruments,
                           "const_module": constellations.single_test}
         self.const = None
+        self.ref_time = pysat.instruments.pysat_testing._test_dates['']['']
 
     def teardown(self):
         """Clean up after each test
         """
-        del self.const, self.instruments, self.in_kwargs
+        del self.const, self.instruments, self.in_kwargs, self.ref_time
 
     @pytest.mark.parametrize("ikey,ival,ilen",
                              [("const_module", None, 1),
@@ -38,6 +39,16 @@ class TestConstellationInit:
             self.in_kwargs[ikey] = ival
         self.const = pysat.Constellation(**self.in_kwargs)
         assert len(self.const.instruments) == ilen
+        return
+
+    def test_init_constellation_bad_inst_module(self):
+        """Test Constellation raises AttributeError with bad inst_module input.
+        """
+        with pytest.raises(AttributeError) as aerr:
+            pysat.Constellation(const_module=self.instruments)
+
+        assert str(aerr).find("missing required attribute 'instruments'")
+        return
 
     def test_construct_raises_noniterable_error(self):
         """Attempt to construct a Constellation by const_module and list
@@ -46,12 +57,14 @@ class TestConstellationInit:
             self.const = pysat.Constellation(instruments=self.instruments[0])
 
         assert str(verr).find("instruments argument must be list-like")
+        return
 
     def test_construct_null(self):
         """Attempt to construct a Constellation with no arguments
         """
         self.const = pysat.Constellation()
         assert len(self.const.instruments) == 0
+        return
 
     def test_getitem(self):
         """Test Constellation iteration through instruments attribute
@@ -60,6 +73,7 @@ class TestConstellationInit:
         self.const = pysat.Constellation(**self.in_kwargs)
         tst_get_inst = self.const[:]
         pysat.utils.testing.assert_lists_equal(self.instruments, tst_get_inst)
+        return
 
     def test_repr_w_inst(self):
         """Test Constellation string output with instruments loaded
@@ -69,6 +83,7 @@ class TestConstellationInit:
         out_str = self.const.__repr__()
 
         assert out_str.find("Constellation(instruments") >= 0
+        return
 
     def test_str_w_inst(self):
         """Test Constellation string output with instruments loaded
@@ -79,15 +94,30 @@ class TestConstellationInit:
 
         assert out_str.find("pysat Constellation ") >= 0
         assert out_str.find("Index Platform") > 0
+        return
 
     def test_str_wo_inst(self):
-        """Test Constellation string output without instruments loaded
+        """Test Constellation string output without instruments.
         """
         self.const = pysat.Constellation()
         out_str = self.const.__str__()
 
         assert out_str.find("pysat Constellation ") >= 0
         assert out_str.find("No assigned Instruments") > 0
+        return
+
+    @pytest.mark.parametrize("common_index,cstr", [(True, "Common"),
+                                                   (False, "Full")])
+    def test_str_with_data(self, common_index, cstr):
+        """Test Constellation string output with loaded data."""
+        self.in_kwargs["common_index"] = common_index
+        self.const = pysat.Constellation(**self.in_kwargs)
+        self.const.load(date=self.ref_time)
+        out_str = self.const.__str__()
+
+        assert out_str.find("pysat Constellation ") >= 0
+        assert out_str.find("{:s} time range".format(cstr)) > 0
+        return
 
     def test_single_attachment_of_custom_function(self):
         """Test successful attachment of custom function
@@ -105,16 +135,17 @@ class TestConstellationInit:
 
         # Add the custom function
         self.const.custom_attach(double_mlt, at_pos='end')
-        self.const.load(2009, 1)
+        self.const.load(date=self.ref_time)
 
         # Test the added value
         for inst in self.const:
             assert 'double_mlt' in inst.variables
             assert (inst['double_mlt'] == 2.0 * inst['mlt']).all()
+        return
 
 
-class TestConstellationAttrs:
-    """Test the Constellation class attributes."""
+class TestConstellationFunc:
+    """Test the Constellation class attributes and methods."""
     def setup(self):
         """Create instruments and a constellation for each test
         """
@@ -140,6 +171,7 @@ class TestConstellationAttrs:
         # Test bounds are the same for all instruments
         for instrument in self.const:
             assert instrument.bounds == self.const.bounds
+        return
 
     def test_empty_data_index(self):
         """ Test the empty index attribute."""
@@ -184,12 +216,19 @@ class TestConstellationAttrs:
         assert not self.const.empty
         return
 
-    def test_full_data_index(self):
+    @pytest.mark.parametrize("ikwarg", [{"common_index": False},
+                                        {"index_res": 60.0}])
+    def test_full_data_index(self, ikwarg):
         """ Test the empty index attribute."""
-        # Test the attribute with loaded data
+        # Test the attribute with loaded data        
+        self.const = pysat.Constellation(instruments=self.inst, **ikwarg)
         self.const.load(date=self.ref_time)
         assert isinstance(self.const.index, pds.Index)
         assert self.const.index[0] == self.ref_time
+
+        if "index_res" in ikwarg.keys():
+            assert self.const.index.freq == pds.DateOffset(
+                seconds=ikwarg['index_res'])
         return
 
     def test_today_yesterday_and_tomorrow(self):
@@ -217,4 +256,11 @@ class TestConstellationAttrs:
         assert len(self.const.variables) > 0
         assert 'uts_pysat_testing' in self.const.variables
         assert 'x' in self.const.variables
+        return
+
+    def test_download(self):
+        """Check that instruments are downloadable."""
+        self.const.download(self.ref_time, self.ref_time)
+        for inst in self.const.instruments:
+            assert len(inst.files.files) > 0
         return
