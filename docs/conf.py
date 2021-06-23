5babe00ab906e6f35e6ad0806c91d452b9f034fc
Russell Stoneback <rstoneba@utdallas.edu>
2021-06-23 17:27:33 -0500

    STY: Reviewer comment, fewer lines

--- docs/conf.py
+++ docs/conf.py
@@ -128,9 +128,8 @@ latex_elements = {}
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, '{:s}.tex'.format(project), title, author, 'manual'),
-]
+latex_documents = [(master_doc, '{:s}.tex'.format(project), title, author,
+                    'manual'),]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
