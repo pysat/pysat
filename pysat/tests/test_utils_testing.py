82c340ecb4d385411eabeb9aee82ebca7c25d265
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2021-07-27 13:24:52 -0400

    DOC: updated docstring style
    Updated the docstring style for the testing utilities unit tests.

--- pysat/tests/test_utils_testing.py
+++ pysat/tests/test_utils_testing.py
@@ -3,8 +3,7 @@
 # Full author list can be found in .zenodo.json file
 # DOI:10.5281/zenodo.1199703
 # ----------------------------------------------------------------------------
-"""
-test the pysat utility testing routines
+"""Tests the pysat utility testing routines
 """
 
 import numpy as np
@@ -14,61 +13,64 @@ from pysat.utils import testing
 
 
 class TestTestingUtils():
+    """Unit tests for `pysat.utils.testing` functions."""
 
     @pytest.mark.parametrize("slist, blist",
                              [([1, 2.0], [1, 2.0, 3]),
                               (['one', 'two'], ['three', 'two', 'one']),
                               ([None, False], [True, False, None])])
     def test_assert_list_contains_good(self, slist, blist):
-        """ Test the successful evaluation of overlapping list contents
-        """
+        """ Test the successful evaluation of overlapping list contents."""
         testing.assert_list_contains(slist, blist)
+        return
 
     @pytest.mark.parametrize("slist, blist",
                              [([1, 4.0], [1, 2.0, 3]),
                               (['one', 'two'], ['three', 'one']),
                               ([None, False], [True, False, "None"])])
     def test_assert_list_contains_bad(self, slist, blist):
-        """ Test raises AssertionError for lists with elements that are unique
+        """ Test raises AssertionError for lists with elements that are unique.
         """
         with pytest.raises(AssertionError) as aerr:
             testing.assert_list_contains(slist, blist)
 
         assert str(aerr).find('not in')
+        return
 
     @pytest.mark.parametrize("slist, blist",
                              [([1, 2.0], [1, 2.0]),
                               (['one', 'two'], ['two', 'one']),
                               ([None, True, False], [True, False, None])])
     def test_assert_list_equal_good(self, slist, blist):
-        """ Test the evaluation of lists with unordered but identical values
+        """ Test the evaluation of lists with unordered but identical values.
         """
         testing.assert_lists_equal(slist, blist)
+        return
 
     @pytest.mark.parametrize("slist, blist",
                              [([1, 4.0], [1, 2.0, 3]),
                               (['one', 'two'], ['three', 'one']),
                               ([None, False], [True, False, "None"])])
     def test_assert_list_equal_bad(self, slist, blist):
-        """ Test the evaluation of overlapping list contents
-        """
+        """ Test the evaluation of overlapping list contents."""
         with pytest.raises(AssertionError):
             testing.assert_lists_equal(slist, blist)
+        return
 
     @pytest.mark.parametrize("val1, val2", [(0.0, 0), (np.nan, np.nan),
                                             ('one', 'one'), (None, None),
                                             (True, 1), (False, 0),
                                             (np.inf, np.inf), (10, 10)])
     def test_nan_equal_good(self, val1, val2):
-        """Test successful evaluation of equivalent values
-        """
+        """Test successful evaluation of equivalent values."""
         assert testing.nan_equal(val1, val2)
+        return
 
     @pytest.mark.parametrize("val1, val2", [(0.0, 1.0), (np.nan, np.inf),
                                             ('one', 'One'), (None, False),
                                             (True, 'true'), (False, 'F'),
                                             (np.inf, -np.inf), (1, 11)])
     def test_nan_equal_bad(self, val1, val2):
-        """Test successful evaluation of un-equivalent values
-        """
+        """Test successful evaluation of un-equivalent values."""
         assert not testing.nan_equal(val1, val2)
+        return
