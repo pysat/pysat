16c140e73cd6b434feafb94c984fbf9f8f63706d
Jeff Klenzing <jklenzing@gmail.com>
2021-08-03 10:09:53 -0400

    DOC: add license info

--- pysat/tests/test_orbits.py
+++ pysat/tests/test_orbits.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# Full license can be found in License.md
+# Full author list can be found in .zenodo.json file
+# DOI:10.5281/zenodo.1199703
+# ----------------------------------------------------------------------------
 """Test the pysat routines for the orbits class."""
 
 import datetime as dt
