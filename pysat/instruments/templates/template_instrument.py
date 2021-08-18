45c4437933e2446f9309423bdbd977934235ff1b
Jeff Klenzing <jklenzing@gmail.com>
2021-08-18 13:06:02 -0400

    DOC: update template instrument

--- pysat/instruments/templates/template_instrument.py
+++ pysat/instruments/templates/template_instrument.py
@@ -311,8 +311,13 @@ def load(fnames, tag=None, inst_id=None, custom_keyword=None):
 
     Note
     ----
-    Any additional keyword arguments passed to pysat.Instrument
-    upon instantiation are passed along to this routine.
+    - Any additional keyword arguments passed to `pysat.Instrument` upon
+      instantiation are passed along to this routine.
+    - When using `pysat.utils.load_netcdf4` for xarray data, pysat will
+      use `decode_timedelta=False` to prevent automated conversion of data
+      to `dt.timedelta` objects if the units attribute is time-like ('hours',
+      'minutes', etc).  This can be added as a custom keyword if timedelta
+      conversion is desired.
 
     Examples
     --------
