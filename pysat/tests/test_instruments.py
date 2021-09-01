96f0dbf3120d6daf227daaab5f710175cdadc379
Jeff Klenzing <jklenzing@gmail.com>
2021-09-01 14:58:08 -0400

    STY: create function to apply new marks to instrument lists

--- pysat/tests/test_instruments.py
+++ pysat/tests/test_instruments.py
@@ -40,28 +40,7 @@ user_info = {'pysat_testing': {'user': 'pysat_testing',
 #                                        user_info=user_info)
 instruments = generate_instrument_list(inst_loc=pysat.instruments,
                                        user_info=user_info)
-
-# The following lines apply the custom instrument lists to each type of test
-method_list = [func for func in dir(InstTestClass)
-               if callable(getattr(InstTestClass, func))]
-# Search tests for iteration via pytestmark, update instrument list
-for method in method_list:
-    if hasattr(getattr(InstTestClass, method), 'pytestmark'):
-        # Get list of names of pytestmarks
-        Nargs = len(getattr(InstTestClass, method).pytestmark)
-        names = [getattr(InstTestClass, method).pytestmark[j].name
-                 for j in range(0, Nargs)]
-        # Add instruments from your library
-        if 'all_inst' in names:
-            mark = pytest.mark.parametrize("inst_name", instruments['names'])
-            getattr(InstTestClass, method).pytestmark.append(mark)
-        elif 'download' in names:
-            mark = pytest.mark.parametrize("inst_dict", instruments['download'])
-            getattr(InstTestClass, method).pytestmark.append(mark)
-        elif 'no_download' in names:
-            mark = pytest.mark.parametrize("inst_dict",
-                                           instruments['no_download'])
-            getattr(InstTestClass, method).pytestmark.append(mark)
+InstTestClass.apply_marks_to_instruments(InstTestClass, instruments=instruments)
 
 
 class TestInstruments(InstTestClass):
