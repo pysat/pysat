b9e01601a273d59d621a104298e237a16e2f8ebe
Jeff Klenzing <jklenzing@gmail.com>
2021-09-13 11:08:55 -0400

    DOC: update documentation

--- pysat/_instrument.py
+++ pysat/_instrument.py
@@ -1147,7 +1147,7 @@ class Instrument(object):
             directory_format, file_format, multi_file_day, orbit_info, and
             pandas_format
         test attributes
-            _download_test, _download_test_travis, and _password_req
+            _test_download, _test_download_ci, and _password_req
 
         """
         # Declare the standard Instrument methods and attributes
