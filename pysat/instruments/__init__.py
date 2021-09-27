120e2b3e85ed118f73b0168d0ca60cabbd634e60
Russell Stoneback <rstoneba@utdallas.edu>
2021-09-27 16:46:26 -0500

    STY: Cleaned up removed model

--- pysat/instruments/__init__.py
+++ pysat/instruments/__init__.py
@@ -5,7 +5,7 @@ Each instrument is contained within a subpackage of this set.
 """
 
 __all__ = ['pysat_testing', 'pysat_testing_xarray', 'pysat_testing2d',
-           'pysat_testing2d_xarray', 'pysat_testmodel', 'pysat_testmodel2']
+           'pysat_testing2d_xarray', 'pysat_testmodel']
 
 for inst in __all__:
     exec("from pysat.instruments import {x}".format(x=inst))
