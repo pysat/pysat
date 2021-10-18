c2ee9de9c7bfbf9dfe3b0a00421e5f12d7166699
Russell Stoneback <rstoneba@utdallas.edu>
2021-10-18 17:40:40 -0500

    STY: Added logger feedback when not assigning metadata

--- pysat/_instrument.py
+++ pysat/_instrument.py
@@ -3008,6 +3008,10 @@ class Instrument(object):
 
                 if warn_default:
                     warnings.warn(default_warn, stacklevel=2)
+            else:
+                estr = ''.join(('Metadata was not assigned as there was ',
+                                'no data returned.'))
+                pysat.logger.INFO(estr)
 
         # Check if load routine actually returns meta
         if self.meta.data.empty:
