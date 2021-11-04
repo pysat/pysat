fe006b79a8048dba3950ec995cb6920f02d0c4d4
Angeline Burrell <aburrell@users.noreply.github.com>
2021-11-04 09:55:47 -0400

    DOC: improved docstring
    Improved the prep_dir function docstring.

--- pysat/utils/testing.py
+++ pysat/utils/testing.py
@@ -163,7 +163,7 @@ def prep_dir(data_path):
     Returns
     -------
     bool
-        True if directories create, False if not
+        True if the directories were created, False if not for any reason (including current existence).
 
     """
     # Create data directories, if possible
