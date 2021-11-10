1e0013350116211516ab2c4ce4c5d7bf8f4261b8
Russell Stoneback <rstoneba@utdallas.edu>
2021-11-10 07:54:14 -0600

    STY: final import round

--- pysat/utils/testing.py
+++ pysat/utils/testing.py
@@ -9,9 +9,9 @@ import numpy as np
 import os
 import time
 
+from pysat.utils.files import check_and_make_path
 from pysat.utils import NetworkLock
 from pysat.utils import time as putime
-from pysat.utils.files import check_and_make_path
 
 
 def assert_list_contains(small_list, big_list, test_nan=False, test_case=True):
