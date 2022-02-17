1be3d64f365565405d878d3682d0d2067d9aeeea
Jeff Klenzing <jklenzing@gmail.com>
2022-02-17 13:31:27 -0500

    DOC: remove comment

--- pysat/tests/test_utils_io.py
+++ pysat/tests/test_utils_io.py
@@ -278,10 +278,6 @@ class TestLoadNetCDF(object):
 
         """
 
-        # TODO(#947) Expand to xarray objects once functionality is updated
-        if not self.testInst.pandas_format:
-            pytest.skip('Not yet implemented for xarray')
-
         # Create a bunch of files by year and doy
         outfile = os.path.join(self.testInst.files.data_path,
                                'pysat_test_ncdf.nc')
