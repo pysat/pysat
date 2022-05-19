BUG: fixed volume scaling tests
Fixed the unit tests for unit scaling for volumes, and added better error messages for unit scaling evaluations.

pysat/tests/test_utils.py
@@ -107,30 +107,35 @@ class TestScaleUnits(object):
 
         """
 
+        estr = "bad {:s} comparison for output unit {:s}".format(scale_type,
+                                                                 out_unit)
+
         if scale_type.lower() == 'angles':
             if out_unit.find("deg") == 0:
-                assert self.scale == 1.0
+                assert self.scale == 1.0, estr
             elif out_unit.find("rad") == 0:
-                assert self.scale == np.pi / 180.0
+                assert self.scale == np.pi / 180.0, est
             else:
-                assert self.scale == 1.0 / 15.0
+                assert self.scale == 1.0 / 15.0, estr
         elif scale_type.lower() == 'distance':
             if out_unit == "m":
-                assert self.scale == 1.0
+                assert self.scale == 1.0, estr
             elif out_unit.find("km") == 0:
-                assert self.scale == 0.001
+                assert self.scale == 0.001, estr
             else:
-                assert self.scale == 100.0
+                assert self.scale == 100.0, estr
         elif scale_type.lower() == 'velocity':
             if out_unit.find("m") == 0:
-                assert self.scale == 1.0
+                assert self.scale == 1.0, estr
             elif out_unit.find("km") == 0:
-                assert self.scale == 0.001
+                assert self.scale == 0.001, estr
         elif scale_type.lower() == 'volume':
-            if out_unit.find("m") == 0:
-                assert self.scale == 1.0
+            if out_unit.find("cm") >= 0 or out_unit.find("cc") > 0:
+                assert self.scale == 1000000.0, estr
+            elif out_unit.find("km") >= 0:
+                assert self.scale == 1.0e-9, estr
             else:
-                assert self.scale == 1000000.0
+                assert self.scale == 1.0, estr
         return
 
     def test_scale_units_same(self):