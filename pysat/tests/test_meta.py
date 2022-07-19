9b410ebf40ffadf9fd65b74a8854fe0a82865212
Russell Stoneback <rstoneba@utdallas.edu>
2022-07-19 13:32:33 -0500

    BUG: Removed TODO and corrected check to >=

--- pysat/tests/test_meta.py
+++ pysat/tests/test_meta.py
@@ -1740,9 +1740,8 @@ class TestMeta(object):
                                        list(self.meta[dvar].children.keys()))
 
             for cvar in concat_meta[dvar].children.keys():
-                # TODO(#911): will either fix this or require warning test
                 assert self.meta[dvar].children[
-                    cvar, self.meta.labels.units].find('Updated') > 0
+                    cvar, self.meta.labels.units].find('Updated') >= 0
         return
 
     # TODO(#789): remove tests for higher order meta
