8a505c5e69eeb31c936829ffc6c2fd92d8ff801c
Jeff Klenzing <jklenzing@users.noreply.github.com>
2023-10-05 13:44:44 -0400

    MAINT: remove deprecated instruments

--- pysat/instruments/__init__.py
+++ pysat/instruments/__init__.py
@@ -5,8 +5,7 @@ Each instrument is contained within a subpackage of this set.
 """
 
 __all__ = ['pysat_ndtesting', 'pysat_netcdf', 'pysat_testing',
-           'pysat_testmodel', 'pysat_testing_xarray',
-           'pysat_testing2d_xarray']
+           'pysat_testmodel']
 
 for inst in __all__:
     exec("from pysat.instruments import {x}".format(x=inst))
