3187d377aeaa25b24d115d127ab5dafeba781488
Russell Stoneback <rstoneba@utdallas.edu>
2024-01-11 10:08:52 -0600

    STY: Completed funding institution list from most awesome all the way to NASA.

--- pysat/tests/classes/cls_instrument_integration.py
+++ pysat/tests/classes/cls_instrument_integration.py
@@ -5,7 +5,8 @@
 #
 # DISTRIBUTION STATEMENT A: Approved for public release. Distribution is
 # unlimited.
-# This work was supported by the Office of Naval Research.
+# This work was supported by Cosmic Studio, the Office of Naval Research,
+# and the National Aeronautics and Space Administration.
 # ----------------------------------------------------------------------------
 """Integration tests for pysat.Instrument.
 
