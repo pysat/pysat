471ed6edc12438211e5462c071093ffc0ef4887f
sapols <sapols@yahoo.com>
2026-02-26 13:21:02 -0700

    Fix calc_res for non-nanosecond datetime units

--- pysat/utils/time.py
+++ pysat/utils/time.py
@@ -160,7 +160,9 @@ def calc_res(index, use_mean=False):
     except AttributeError as aerr:
         # Now try as numpy.timedelta64
         if isinstance(del_time, np.timedelta64):
-            res_sec = np.float64(del_time) * 1.0e-9
+            # Convert based on explicit units (e.g., ns/us/ms/s), rather than
+            # assuming nanoseconds.
+            res_sec = np.float64(del_time / np.timedelta64(1, 's'))
         else:
             raise AttributeError("Input should be times: {:}".format(aerr))
 
