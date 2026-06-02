606f4d60b59fe45ac7b54cc51cbf697a84458e04
Russell Stoneback <rstoneba@utdallas.edu>
2026-06-02 16:11:02 -0500

    TST: Back to original settings

--- pysat/utils/_core.py
+++ pysat/utils/_core.py
@@ -619,16 +619,16 @@ def update_fill_values(inst, variables=None, new_fill_val=np.nan):
                 try:
                     if np.isnan(old_fill_val):
                         # Needed for NaNs
-                        ifill, = np.where(np.isnan(inst[var].values))
+                        ifill = np.where(np.isnan(inst[var].values))
                     else:
                         # Catches numbers that fail gracefully from NaN check
-                        ifill, = np.where(inst[var].values == old_fill_val)
+                        ifill = np.where(inst[var].values == old_fill_val)
                 except TypeError:
                     # This catches strings and objects
-                    ifill, = np.where(inst[var].values == old_fill_val)
+                    ifill = np.where(inst[var].values == old_fill_val)
 
                 if len(ifill) > 0:
-                    inst[ifill, var] = new_fill_val
+                    inst[var].values[ifill] = new_fill_val
 
     return
 
