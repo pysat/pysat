2a92f21dfa3b0bbc99fa9c78ec9904891c715de0
Russell Stoneback <rstoneba@utdallas.edu>
2026-06-01 19:11:27 -0500

    TST: Confirmed that #974 in effect

--- pysat/tests/test_utils_io.py
+++ pysat/tests/test_utils_io.py
@@ -302,19 +302,6 @@ class TestLoadNetCDF(object):
 
         del netcdf_inst.data, netcdf_inst
 
-        # Debug check to confirm when a windows file issue is sorted
-        os.remove(outfile)
-        # try:
-        #     os.remove(outfile)
-        # except Exception as err:
-        #     # Problems expected in windows
-        #     if platform.system() != 'Windows':
-        #         raise err
-        # else:
-        #     # When problem is fixed in windows
-        #     if platform.system() == 'Windows':
-        #         raise OSError("Windows can correctly manage files! See #974")
-
         return
 
     def test_write_netcdf4_duplicate_variable_names(self):
