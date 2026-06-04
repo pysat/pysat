2b4012cb20290d86d9f94fe4dd8d6f30c3d38ea1
Russell Stoneback <rstoneba@utdallas.edu>
2026-06-03 19:09:51 -0500

    BUG: PEP8!!!

--- pysat/utils/_core.py
+++ pysat/utils/_core.py
@@ -642,7 +642,7 @@ def update_fill_values(inst, variables=None, new_fill_val=np.nan):
                         # which works, but takes forever for some reason on
                         # at least one test
                         # (test_update_fill_values_by_type[testmodel]).
-                        # TODO (#TBD) Sort out why *ifill is lacing performance
+                        # TODO(#TBD) Sort out why *ifill is lacing performance
                         inst[var].values[ifill] = new_fill_val
 
     return
